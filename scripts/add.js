e93d0ecc1e7da0bbb2d54fe8ec49ea9da5533846
lachlanjc <lachlanjc527@yahoo.com>
2014-04-16 21:04:06 -0400

    new fonts + huge update

--- scripts/add.js
+++ scripts/add.js
@@ -2,3 +2,9 @@ function addIngredient() {
   var list = document.getElementById("ingredients")
   list.innerHTML += '<input type="text" placeholder="Ingredient">'
 }
+var hidden-input = document.getElementById('hidden-input');
+hidden-input.onfocus = hidden-input.blur;
+mike.onwebkitspeechchange = function(e) {
+    //console.log(e); // SpeechInputEvent
+    document.getElementById('instructions').value = hidden-input.value;  
+};
