ec45e6de0ca30282acd6e42c52bf6a24fd0d61b0
Lachlan C <lachlan.campbell527@gmail.com>
2014-04-22 10:52:32 -0400

    next update

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -4,9 +4,14 @@ var inputter = document.getElementById("inputter");
 function render() {
   var text = inputter.value;
   var ret = converter.makeHtml(text);
-  document.getElementById('rendered').innerHTML = ret;  
+  document.getElementById('rendered').innerHTML = ret;
 }
 function save() {
     var text = inputter.value;
     // Save text to Evernote
+}
+
+function goBack() {
+  var history = window.history;
+  history.back();
 }
