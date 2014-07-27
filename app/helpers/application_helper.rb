a9e234fdd29a1b5c86ae0875374669faf553e02d
lachlanjc <lachlan.campbell@icloud.com>
2014-07-27 15:30:57 -0400

    Flash alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_sym
+      when :notice then "alert alert-info"
+      when :success then "alert alert-success"
+      when :error then "alert alert-danger"
+      when :alert then "alert alert-danger"
+    end
+  end
+
+  def flash_icon_class(level)
+    case level.to_sym
+      when :notice then "geomicon-info geomicon"
+      when :success then "geomicon-check geomicon"
+      when :error then "geomicon-alert geomicon"
+      when :alert then "geomicon-alert geomicon"
+    end
+  end
 end
