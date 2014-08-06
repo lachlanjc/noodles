e7693c74dfe472a82403133c8f709dce790e8bae
lachlanjc <lachlan.campbell@icloud.com>
2014-08-06 11:29:23 -0400

    Much improved notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
   def flash_class(level)
     case level.to_sym
-      when :notice then "alert alert-info"
-      when :success then "alert alert-success"
-      when :error then "alert alert-danger"
-      when :alert then "alert alert-danger"
+      when :notice then "notification-info"
+      when :success then "notification-success"
+      when :error then "notification-danger"
+      when :alert then "notification-danger"
     end
   end
 
   def flash_icon_class(level)
     case level.to_sym
-      when :notice then "geomicon-info geomicon"
-      when :success then "geomicon-check geomicon"
-      when :error then "geomicon-alert geomicon"
-      when :alert then "geomicon-alert geomicon"
+      when :notice then "geomicon-info"
+      when :success then "geomicon-check"
+      when :error then "geomicon-alert"
+      when :alert then "geomicon-alert"
     end
   end
 end
