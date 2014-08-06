fdd912cf7eecf125aa41930b367a242ea1b9bf72
lachlanjc <lachlan.campbell@icloud.com>
2014-08-06 11:29:46 -0400

    Add name and avatars

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
 
   root 'pages#welcome'
 
+  get '/home' => 'pages#welcome'
+
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions"}, skip: [:sessions, :registrations]
 
   as :user do
-    get    "sign_in"  => "devise/sessions#new",         as: :new_user_session
+    get    "sign_in"  => "devise/sessions#new",         as: :create_user_session
     post   "sign_in"  => "devise/sessions#create",      as: :user_session
     get    "sign_out" => "devise/sessions#destroy",     as: :destroy_user_session
 
-    get    "sign_up" => "devise/registrations#new",     as: :new_user_registration
+    get    "sign_up" => "devise/registrations#new",     as: :add_user
     post   "sign_up" => "devise/registrations#create",  as: :user_registration
     put    "sign_up" => "devise/registrations#update",  as: :update_user_registration
     delete "sign_up" => "devise/registrations#destroy", as: :destroy_user
