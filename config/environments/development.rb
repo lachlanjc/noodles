a28b58bb4cc3aed5e862afcc0179b2949584139f
Taran <tns12@scasd.org>
2014-08-12 09:23:31 -0400

    Added confirmable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
   
   # Settings specified here will take precedence over those in config/application.rb.
 
