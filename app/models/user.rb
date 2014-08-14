fb29c5cb58435186722a89d78195a90c55772710
lachlanjc <lachlan.campbell@icloud.com>
2014-08-14 09:19:12 -0400

    Recipes may belong to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  has_many :recipes
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   include Gravtastic
   gravtastic
 end
