bc724fc304f7f9a473c1e37e43c86ac25e334ce1
lachlanjc <lachlan.campbell@icloud.com>
2014-08-23 23:23:00 -0400

    Recipes are private, once and for all
    It’s all fixed, finally.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,12 +3,21 @@ class RecipesController < ApplicationController
 
   # GET /recipes
   def index
-    @recipes = Recipe.accessible_by(current_ability, params[:update].to_sym)
+    if current_user
+      @recipes = Recipe.where(:user_id => current_user.id)
+    else
+      render "home"
+    end
   end
 
   # GET /recipes/1
   def show
-    @recipe = Recipe.find(params[:id])
+    if @recipe.user_id == current_user.id
+      set_recipe
+    else
+      flash[:error] = "You are not allowed to look at that recipe."
+      redirect_to root_url
+    end
   end
 
   # GET /recipes/new
@@ -18,14 +27,18 @@ class RecipesController < ApplicationController
 
   # GET /recipes/1/edit
   def edit
+    if @recipe.user_id == current_user.id
+      set_recipe
+    else
+      flash[:error] = "You are not allowed to edit that recipe."
+      redirect_to root_url
+    end
   end
 
   # POST /recipes
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.user = current_user
-
-    # authorize! :create, @recipe
+    @recipe.user_id = current_user.id
 
     if @recipe.save
       flash[:success] = "Your recipe has been created."
@@ -37,10 +50,6 @@ class RecipesController < ApplicationController
 
   # PATCH/PUT /recipes/1
   def update
-    @recipe = Recipe.find(params[:id])
-    @recipe.assign_attributes(task_params)
-    authorize! :update, @recipe
-
     if @recipe.update(recipe_params)
       flash[:success] = "Awesome, your changes have been saved."
       redirect_to @recipe
@@ -51,24 +60,12 @@ class RecipesController < ApplicationController
 
   # DELETE /recipes/1
   def destroy
-    @recipe = Recipe.find(params[:id])
-    authorize! :destroy, @recipe
-
     @recipe.destroy
-    @recipes = Task.accessible_by(current_ability)
     flash[:alert] = "Your recipe has been deleted."
     redirect_to recipes_url
   end
 
   private
-
-    def user_is_current_user
-      unless Recipe.find(params[:user_id])  == current_user.id
-        flash[:notice] = "You may only view your own recipes."
-        redirect_to root_path
-      end
-    end
-
     # Use callbacks to share common setup or constraints between actions.
     def set_recipe
       @recipe = Recipe.find(params[:id])
@@ -76,6 +73,6 @@ class RecipesController < ApplicationController
 
     # Only allow trusted parameters
     def recipe_params
-      params.require(:recipe).permit(:title, :description, :ingredients, :instructions, :user_id)
+      params.require(:recipe).permit(:title, :description, :ingredients, :instructions)
     end
 end
