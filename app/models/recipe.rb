b2e37182bbe8cb8f527df6c4b1531080ac9fd712
lachlanjc <lachlan.campbell@icloud.com>
2014-09-15 18:54:28 -0400

    Photo size improvements

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,11 +2,10 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :image, styles: {
-    thumb: '100x100>',
-    recipe: '1024x768>',
-    medium: '300x300>'
+    thumb: '250x250>',
+    img: '1024x768>'
   }
   validates :avatar,
   attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
-  attachment_size: { less_than: 3.megabytes }
+  attachment_size: { less_than: 2.megabytes }
 end
