080296272eaf3eac7410b23b87e07281f2ee6da1
lachlanjc <lachlan.campbell@icloud.com>
2014-09-24 16:19:24 -0400

    Remove file size limit

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,5 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :img
-  validates_attachment_content_type :img, :size => { less_than: => 2.megabytes }, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :img, :content_type => /\Aimage\/.*\Z/
 end
