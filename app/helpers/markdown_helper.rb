6939ae449475ede25c29e98971d56698fa07f40c
lachlanjc <lachlan.campbell@icloud.com>
2014-09-25 06:34:35 -0400

    Add Markdown options

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -2,7 +2,7 @@ require 'redcarpet'
 
 module MarkdownHelper
   def markdown(str)
-    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, underline: true, space_after_headers: true, strikethrough: true)
     rendered = md.render(str)
     return rendered.html_safe
   end
