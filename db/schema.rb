e9266313e7682e53bd95895ab19f4c02c0e8bcbc
Taran <tns12@scasd.org>
2014-09-27 20:23:06 -0400

    Merged-filter-and-navbars

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925212751) do
+ActiveRecord::Schema.define(version: 20140925233303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
