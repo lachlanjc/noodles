c027976da447d90a67813ab7bab10d60500e522b
lachlanjc <lachlan.campbell@icloud.com>
2014-10-10 17:56:36 -0400

    New flash classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,11 @@
 module ApplicationHelper
   def flash_class(level)
     case level.to_sym
-      when :notice then "notification-info"
-      when :success then "notification-success"
-      when :error then "notification-danger"
-      when :alert then "notification-danger"
-      when :view then "notification-info"
-    end
-  end
-
-  def flash_icon_class(level)
-    case level.to_sym
-      when :notice then "geomicon-info"
-      when :success then "geomicon-check"
-      when :error then "geomicon-alert"
-      when :alert then "geomicon-alert"
-      when :view then "geomicon-view"
+      when :notice then "flash-info"
+      when :success then "flash-success"
+      when :error then "flash-danger"
+      when :alert then "flash-danger"
+      when :view then "flash-danger"
     end
   end
 end
