10f5467ad61eb05a683255d85f21a4fa7b21f057
lachlanjc <lachlan.campbell@icloud.com>
2014-10-17 13:24:23 -0400

    New recipe URLs

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,9 @@ class Recipe < ActiveRecord::Base
 
   has_attached_file :img,
     :path => 'recipes/:id/img/:style.:extension'
-
   validates_attachment_content_type :img, :content_type => /\Aimage\/.*\Z/
+
+  def to_param
+    "#{id} #{title}".parameterize
+  end
 end
