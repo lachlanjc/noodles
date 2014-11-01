6c223ee85af9588bd1998d931c5e0e4910637137
lachlanjc <lachlan.campbell@icloud.com>
2014-11-01 12:06:21 -0400

    Better route names

--- config/routes.rb
+++ config/routes.rb
@@ -19,15 +19,16 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions"}, skip: [:sessions, :registrations]
 
   as :user do
-    get    "sign_in"  => "devise/sessions#new",         as: :create_user_session
-    post   "sign_in"  => "devise/sessions#create",      as: :user_session
-    get    "sign_out" => "devise/sessions#destroy",     as: :destroy_user_session
+    get    'sign_in'  => 'devise/sessions#new',         as: :sign_in
+    post   'sign_in'  => 'devise/sessions#create',      as: :update_user_session
+    get    'sign_out' => 'devise/sessions#destroy',     as: :sign_out
 
-    get    "sign_up" => "devise/registrations#new",     as: :add_user
-    post   "sign_up" => "devise/registrations#create",  as: :user_registration
-    put    "sign_up" => "devise/registrations#update",  as: :update_user_registration
-    delete "sign_up" => "devise/registrations#destroy", as: :destroy_user
-    get    "account" => "devise/registrations#edit",    as: :edit_user_registration
+    get    'sign_up' => 'devise/registrations#new',     as: :sign_up
+    post   'sign_up' => 'devise/registrations#create',  as: :add_user
+    put    'sign_up' => 'devise/registrations#update',  as: :update_user_registration
+    delete 'sign_up' => 'devise/registrations#destroy', as: :destroy_user
+
+    get    'settings' => 'devise/registrations#edit',    as: :settings
   end
 
   get '/admin/all_users' => 'analytics#all_users', as: :analytics_users
