aef781bfc84a6c49d06e439119fd27a424e7a290
lachlanjc <lachlan.campbell@icloud.com>
2014-11-02 14:43:40 -0500

    Paperclip default URL: ''

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,8 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :img,
-    :path => 'recipes/:id/img/:style.:extension'
+    :path => 'recipes/:id/img/:style.:extension',
+    :default_url => ''
   validates_attachment_content_type :img, :content_type => /\Aimage\/.*\Z/
 
   def to_param
