5cc1f432d23a6615e5d78b2818eab79586e5ef79
lachlanjc <lachlan.campbell@icloud.com>
2014-11-17 19:39:56 -0500

    Much cleaner analytics controller

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,48 +1,31 @@
 class AnalyticsController < ApplicationController
-  def check_access(id)
-    if id == 1 or id == 23
+  before_filter :authenticate
+
+  def authenticate
+    if current_user && (current_user.id == 1 || current_user.id == 23)
       @access = true
     else
       @access = false
-    end
-  end
-
-  def authenticate
-    if current_user
-      check_access(current_user.id)
-    else
       flash[:danger] = "Sorry, you can't look at that page."
       redirect_to root_url
     end
   end
 
   def recipes
-    authenticate
     if @access == true
       @recipes = Recipe.all.order(created_at: :desc)
-    else
-      flash[:danger] = "You're not allowed to see those."
-      redirect_to root_url
     end
   end
 
   def all_users
-    authenticate
     if @access == true
       @users = User.all.order(created_at: :desc)
-    else
-      flash[:danger] = "You're not allowed to see those."
-      redirect_to root_url
     end
   end
 
   def marketable
-    authenticate
     if @access == true
       @users = User.all.order(created_at: :desc)
-    else
-      flash[:danger] = "You're not allowed to see those."
-      redirect_to root_url
     end
   end
 end
