ddf528b537d3735004798070b077f9c3d8363923
lachlanjc <lachlan.campbell@icloud.com>
2014-11-17 21:26:38 -0500

    Protect authenticate method in analytics cntrlr

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,16 +1,6 @@
 class AnalyticsController < ApplicationController
   before_filter :authenticate
 
-  def authenticate
-    if current_user && (current_user.id == 1 || current_user.id == 23)
-      @access = true
-    else
-      @access = false
-      flash[:danger] = "Sorry, you can't look at that page."
-      redirect_to root_url
-    end
-  end
-
   def recipes
     if @access == true
       @recipes = Recipe.all.order(created_at: :desc)
@@ -28,4 +18,15 @@ class AnalyticsController < ApplicationController
       @users = User.all.order(created_at: :desc)
     end
   end
+
+  private
+    def authenticate
+      if current_user && (current_user.id == 1 || current_user.id == 23)
+        @access = true
+      else
+        @access = false
+        flash[:danger] = "Sorry, you can't look at that page."
+        redirect_to root_url
+      end
+    end
 end
