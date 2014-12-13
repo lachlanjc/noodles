463c840891fd009d21a51dde9300d1687c18c6b7
lachlanjc <lachlan.campbell@icloud.com>
2014-12-13 12:14:06 -0500

    Page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
   def flash_class(level)
     case level.to_sym
-      when :info then "flash-info"
-      when :success then "flash-success"
-      when :danger then "flash-danger"
-      when :view then "flash-danger"
+      when :info then 'flash-info'
+      when :success then 'flash-success'
+      when :danger then 'flash-danger'
+      when :view then 'flash-danger'
     end
+  end
+
+  def title(page_title)
+    content_for(:title) { page_title }
   end
 end
