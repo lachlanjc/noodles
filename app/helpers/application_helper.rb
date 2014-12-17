1c664301a60ccfe0852f5e23c6b86745bf070211
lachlanjc <lachlan.campbell@icloud.com>
2014-12-16 20:12:42 -0500

    New About/Help/Documentation pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,9 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def markdown(str)
+    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, underline: true, space_after_headers: true, strikethrough: true)
+    return md.render(str).html_safe
+  end
 end
