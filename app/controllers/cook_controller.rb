0288cd5b72afb58d2ea45bfb311aa98c2fa2fbc8
lachlanjc <lachlan.campbell@icloud.com>
2014-12-20 13:53:46 -0500

    Cleaner cook controller

--- app/controllers/cook_controller.rb
+++ app/controllers/cook_controller.rb
@@ -1,9 +1,12 @@
 class CookController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @recipe = Recipe.find(params[:recipe_id])
 
-    if current_user && @recipe.user_id == current_user.id
+    if @recipe.user_id == current_user.id
       @recipe_step_count = @recipe.instructions.lines.count
+      render action: 'index'
     else
       flash[:view] = "No cooking with recipes that aren't yours!"
       redirect_to root_url
