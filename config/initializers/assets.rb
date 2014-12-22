e9bc4f269d662095070d75948d20c5d00fd23f55
lachlanjc <lachlan.campbell@icloud.com>
2014-12-22 16:53:06 -0500

    Remove cook.js from precompilation list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,4 @@
 Rails.application.config.assets.precompile += %w( jquery.js )
-Rails.application.config.assets.precompile += %w( cook.js )
 Rails.application.config.assets.precompile += %w( cook.css )
 Rails.application.config.assets.precompile += %w( editor.css )
 Rails.application.config.assets.precompile += %w( editor.js )
