a31e01b5be9f2d8d639c6fc6906164dfca995c85
lachlanjc <lachlan.campbell@icloud.com>
2015-01-11 17:34:57 -0500

    No more app.getnoodl.es :globe_with_meridians:

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'app.getnoodl.es' }
+  config.action_mailer.default_url_options = { :host => 'noodles.dev' }
   config.action_mailer.delivery_method = :ses
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
     }
   }
 
-  config.action_mailer.default_url_options = { :host => 'app.getnoodl.es' }
+  config.action_mailer.default_url_options = { :host => 'www.getnoodl.es' }
   config.action_mailer.delivery_method = :ses
 
 end
