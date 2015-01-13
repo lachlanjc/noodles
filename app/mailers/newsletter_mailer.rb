2d6afe4ea1a0b5fee3756984a835c015f984acef
lachlanjc <lachlan.campbell@icloud.com>
2015-01-13 09:29:49 -0500

    Newsletter

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -2,7 +2,7 @@ class NewsletterMailer < ActionMailer::Base
   default from: 'lachlan@getnoodl.es'
 
   def newsletter(user)
-    @subject = '[Noodles] Ingredient checklists, remember your experiences, and more.'
+    @subject = '[Noodles] Web Import, Search, better sharing, and more.'
     @id = user.id
     mail(to: user.email, subject: @subject)
   end
