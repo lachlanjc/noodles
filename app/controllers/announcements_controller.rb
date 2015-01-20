69faeef70e5dee1167cfffa54541016604af3a18
lachlanjc <lachlan.campbell@icloud.com>
2015-01-19 22:29:19 -0500

    Announcement cleanup

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,59 +1,51 @@
 class AnnouncementsController < ApplicationController
   before_action :set_announcement, only: [:show, :edit, :update, :destroy]
-  before_filter :check_admin_and_redirect, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :set_admin, only: [:index, :show]
+  before_filter :set_admin_and_redirect, only: [:new, :create, :edit, :update, :destroy]
+
+  include ApplicationHelper
 
   # GET /announcements
   def index
-    check_admin
     @announcements = Announcement.all.order(created_at: :desc)
   end
 
   # GET /announcements/1
   def show
-    check_admin
   end
 
   # GET /announcements/new
   def new
-    if @admin == true
-      @announcement = Announcement.new
-      render :new
-    end
+    @announcement = Announcement.new
+    render :edit
   end
 
   # GET /announcements/1/edit
   def edit
-    if @admin == true
-      render :edit
-    end
   end
 
   # POST /announcements
   def create
-    if @admin == true
-      @announcement = Announcement.new(announcement_params)
-      @announcement.body_rendered = markdown(@announcement.body)
+    @announcement = Announcement.new(announcement_params)
+    @announcement.body_rendered = markdown(@announcement.body)
 
-      if @announcement.save
-        flash[:success] = 'Posted!'
-        redirect_to @announcement
-      else
-        render :new
-      end
+    if @announcement.save
+      flash[:success] = "Posted!"
+      redirect_to @announcement
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /announcements/1
   def update
-    if @admin == true
-      @announcement.body_rendered = markdown(@announcement.body)
+    @announcement.body_rendered = markdown(@announcement.body)
 
-      if @announcement.update(announcement_params)
-        flash[:success] = 'Announcement updated!'
-        redirect_to @announcement
-      else
-        render :edit
-      end
+    if @announcement.update(announcement_params)
+      flash[:success] = "Announcement updated!"
+      redirect_to @announcement
+    else
+      render :edit
     end
   end
 
@@ -61,7 +53,7 @@ class AnnouncementsController < ApplicationController
   def destroy
     if @admin == true
       @announcement.destroy
-      flash[:danger] = 'Announcement deleted.'
+      flash[:danger] = "Announcement deleted."
       redirect_to announcements_url
     end
   end
@@ -70,32 +62,25 @@ class AnnouncementsController < ApplicationController
     user = User.find(params[:user_id])
     user.want_newsletter = false
     user.save
-    flash[:success] = "You've successfully unsubscribed."
+    flash[:success] = "Okay, you've unsubscribed."
     redirect_to root_url
   end
 
   private
-    def check_admin
+    def set_admin
+      @admin = false unless current_user && (current_user.id == 1 || current_user.id == 23)
+    end
+
+    def is_admin?
       if current_user && (current_user.id == 1 || current_user.id == 23)
-        @admin = true
+        return true
       else
-        @admin = false
+        return false
       end
     end
 
-    def check_admin_and_redirect
-      if current_user && (current_user.id == 1 || current_user.id == 23)
-        @admin = true
-      else
-        @admin = false
-        flash[:danger] = "Sorry, you can't access that page."
-        redirect_to root_url
-      end
-    end
-
-    def markdown(str)
-      md = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, underline: true, space_after_headers: true, strikethrough: true)
-      return md.render(str)
+    def set_admin_and_redirect
+      redirect_to root_url unless is_admin?
     end
 
     # Use callbacks to share common setup or constraints between actions.
@@ -105,6 +90,6 @@ class AnnouncementsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def announcement_params
-      params.require(:announcement).permit(:title, :body, :body_rendered, :author_id, :mail)
+      params.require(:announcement).permit(:title, :body, :body_rendered, :author_id)
     end
 end
