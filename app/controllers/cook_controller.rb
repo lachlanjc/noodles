6510651ae67e27d249750044c76452d290467983
lachlanjc <lachlan.campbell@icloud.com>
2015-01-19 22:05:31 -0500

    Public cook mode fix

--- app/controllers/cook_controller.rb
+++ app/controllers/cook_controller.rb
@@ -1,4 +1,7 @@
 class CookController < ApplicationController
+  include ApplicationHelper
+  include RecipesHelper
+
   def index
     @recipe = Recipe.find(params[:recipe_id])
 
@@ -6,7 +9,7 @@ class CookController < ApplicationController
       @recipe_step_count = @recipe.instructions.lines.count
       render :index
     else
-      flash[:view] = "You can't cook with recipes that aren't yours!"
+      flash[:view] = "You can't cook with private recipes!"
       redirect_to root_url
     end
   end
