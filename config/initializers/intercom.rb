d36417c2f19f575693bc6ca8d464513bd2e22030
lachlanjc <lachlan.campbell@icloud.com>
2015-01-20 17:51:39 -0500

    Intercom data FTW! :satellite: :mailbox:

--- config/initializers/intercom.rb
+++ config/initializers/intercom.rb
@@ -1,24 +1,24 @@
 IntercomRails.config do |config|
   # == Intercom app_id
   #
-  config.app_id = ENV['INTERCOM_APP_ID']
+  config.app_id = ENV["INTERCOM_APP_ID"]
 
   # == Intercom secret key
   # This is required to enable secure mode, you can find it on your Intercom
-  # 'security' configuration page.
+  # "security" configuration page.
   #
-  config.api_secret = ENV['INTERCOM_API_SECRET']
+  config.api_secret = ENV["INTERCOM_API_SECRET"]
 
   # == Intercom API Key
   # This is required for some Intercom rake tasks like importing your users;
   # you can generate one at https://app.intercom.io/apps/api_keys.
   #
-  # config.api_key = '...'
+  # config.api_key = "..."
 
   # == Enabled Environments
   # Which environments is auto inclusion of the Javascript enabled for
   #
-  config.enabled_environments = ['development', 'production']
+  config.enabled_environments = ["development", "production"]
 
   # == Current user method/variable
   # The method/variable that contains the logged in user in your controllers.
@@ -35,53 +35,7 @@ IntercomRails.config do |config|
   # A Proc that given a user returns true if the user should be excluded
   # from imports and Javascript inclusion, false otherwise.
   #
-  # config.user.exclude_if = Proc.new { |user| user.deleted? }
-
-  # == User Custom Data
-  # A hash of additional data you wish to send about your users.
-  # You can provide either a method name which will be sent to the current
-  # user object, or a Proc which will be passed the current user.
-  #
-  # config.user.custom_data = {
-  #   :plan => Proc.new { |current_user| current_user.plan.name },
-  #   :favorite_color => :favorite_color
-  # }
-
-  # == User -> Company association
-  # A Proc that given a user returns an array of companies
-  # that the user belongs to.
-  #
-  # config.user.company_association = Proc.new { |user| user.companies.to_a }
-  # config.user.company_association = Proc.new { |user| [user.company] }
-
-  # == Current company method/variable
-  # The method/variable that contains the current company for the current user,
-  # in your controllers. 'Companies' are generic groupings of users, so this
-  # could be a company, app or group.
-  #
-  # config.company.current = Proc.new { current_company }
-
-  # == Company Custom Data
-  # A hash of additional data you wish to send about a company.
-  # This works the same as User custom data above.
-  #
-  # config.company.custom_data = {
-  #   :number_of_messages => Proc.new { |app| app.messages.count },
-  #   :is_interesting => :is_interesting?
-  # }
-
-  # == Company Plan name
-  # This is the name of the plan a company is currently paying (or not paying) for.
-  # e.g. Messaging, Free, Pro, etc.
-  #
-  # config.company.plan = Proc.new { |current_company| current_company.plan.name }
-
-  # == Company Monthly Spend
-  # This is the amount the company spends each month on your app. If your company
-  # has a plan, it will set the 'total value' of that plan appropriately.
-  #
-  # config.company.monthly_spend = Proc.new { |current_company| current_company.plan.price }
-  # config.company.monthly_spend = Proc.new { |current_company| (current_company.plan.price - current_company.subscription.discount) }
+  config.user.exclude_if = Proc.new { |user| user.deleted? }
 
   # == Inbox Style
   # This enables the Intercom inbox which allows your users to read their
