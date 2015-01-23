bf5b91bd0236ced87a05b5f3a67662fd5e3f552d
lachlanjc <lachlan.campbell@icloud.com>
2015-01-23 14:46:19 -0500

    Better :iphone: :globe_with_meridians:

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
-Rails.application.config.assets.precompile += %w( cook.css )
-Rails.application.config.assets.precompile += %w( editor.css )
-Rails.application.config.assets.precompile += %w( editor.js )
+Rails.application.config.assets.precompile += %w(cook.css)
+Rails.application.config.assets.precompile += %w(home.css)
+Rails.application.config.assets.precompile += %w(editor.css)
+Rails.application.config.assets.precompile += %w(editor.js)
