5f3ada37ab5e93136ad3925a334c5336867213b4
lachlanjc <lachlan.campbell@icloud.com>
2015-01-28 00:02:33 -0500

    Recipe lists in React

---
+++ app/assets/javascripts/components/recipe_list.js.jsx
@@ -0,0 +1,28 @@
+var RecipeItem = React.createClass({
+  render: function() {
+    var favoriteData = null;
+    if (this.props.data.favorite === true) {
+      favoriteData = <IconFavorite />;
+    }
+
+    return <a href={this.props.data.url} className="link-reset">
+      <div className="panel">
+        {favoriteData}
+        <h3 className="m0">{this.props.data.title}</h3>
+        <div className="text text-muted">{this.props.data.description}</div>
+      </div>
+    </a>
+  }
+});
+
+var RecipeList = React.createClass({
+  render: function() {
+    return (
+      <div className="recipe-list">
+        {this.props.recipes.map(function(recipe) {
+           return <RecipeItem key={recipe.id} data={recipe} />;
+        })}
+      </div>
+    );
+  }
+});
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
   include SearchCop
+  include MarkdownHelper
 
   search_scope :search do
     attributes :title, :description
@@ -17,5 +18,16 @@ class Recipe < ActiveRecord::Base
 
   def to_param
     "#{id} #{title}".parameterize
+  end
+
+  def as_json
+    {
+      title: title,
+      description: description,
+      favorite: favorite,
+      url: "/recipes/" + id.to_s,
+      notes: notes.to_s,
+      notes_rendered: markdown(notes.to_s)
+    }
   end
 end
