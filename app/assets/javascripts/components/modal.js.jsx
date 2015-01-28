2703ab210a81250a684a92cbd8f87593c710de5b
lachlanjc <lachlan.campbell@icloud.com>
2015-01-28 16:29:33 -0500

    ModalClose in React

---
+++ app/assets/javascripts/components/modal.js.jsx
@@ -0,0 +1,8 @@
+// Actual modals are coming...
+var ModalClose = React.createClass({
+  render: function() {
+    return <a className="modal-close float-right" href="#">
+      <IconClose />
+    </a>
+  }
+});
