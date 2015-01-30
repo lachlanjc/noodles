e15b4016459a364b3484eea64bc0dc1af5046767
lachlanjc <lachlan.campbell@icloud.com>
2015-01-30 17:53:59 -0500

    Use Mandrill in prod as well!
    Oops.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   }
 
   config.action_mailer.default_url_options = { :host => 'www.getnoodl.es' }
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.delivery_method = :mandrill
 
   config.react.variant = :production
 
