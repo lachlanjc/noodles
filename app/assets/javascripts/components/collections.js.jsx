145e68ad1e2ae98f26e966287bf53c749b3179cd
lachlanjc <lachlan.campbell@icloud.com>
2015-01-31 16:52:30 -0500

    Unify CollectionHeaders, close #27 please

--- app/assets/javascripts/components/collections.js.jsx
+++ app/assets/javascripts/components/collections.js.jsx
@@ -28,45 +28,34 @@ var CollectionItem = React.createClass({
     return <a href={this.props.data.url} className="link-reset">
       <div className={"collection-preview rounded shadow mb2 py3" + imgClass} style={imgStyle}>
         <div className="collection-preview-container text-center">
-          <h2 className="collection-name">{this.props.data.name}</h2>
-          <div className="collection-description lead mb1">{this.props.data.description ? this.props.data.description : null}</div>
+          <h2 className="collection-name m0">{this.props.data.name}</h2>
+          <div className="collection-description lead mt1">{this.props.data.description ? this.props.data.description : null}</div>
         </div>
       </div>
     </a>
   }
 });
 
-
-var CollectionHeaderBlank = React.createClass({
+var CollectionHeader = React.createClass({
   render: function() {
-    return <header className="col-7 container text-center">
-      <h1 className="ib m0">{this.props.collection.name}</h1>
+    var photo_url = this.props.collection.photo_url;
+
+    var headerClass, headerStyle;
+    var editBtnClass = "edit-btn";
+    if (photo_url.length > 0) {
+      headerClass = "has-img mb2";
+      editBtnClass = "edit-btn-glass";
+      var headerStyle = {
+        backgroundImage: "url(" + photo_url + ")"
+      }
+    }
+
+    return <header className={"collection-header full-width text-center py3 " + headerClass} style={headerStyle}>
+      <h1 className="ib collection-name m0">{this.props.collection.name}</h1>
       <a href="#editCollection" className="modalTrigger">
-        <IconEdit classes="edit-btn ib" />
+        <IconEdit classes={"ib " + editBtnClass} />
       </a>
-      <div className="text m1-btm">{this.props.collection.description}</div>
+      <div className="lead collection-description">{this.props.collection.description}</div>
     </header>
-  }
-});
-
-var CollectionHeaderImg = React.createClass({
-  render: function() {
-    var collectionHeaderStyle = {
-      backgroundImage: 'url(' + this.props.collection.photo_url + ')'
-    };
-
-    return <div className="collection-header has-img" style={collectionHeaderStyle}>
-      <div className="col-10 mx-auto collection-header-container">
-        <h1 className="collection-name ib">
-          {this.props.collection.name}
-        </h1>
-        <a href="#editCollection" className="modalTrigger">
-          <IconEdit classes="edit-btn-glass ib m-small-left" />
-        </a>
-        <div className="collection-description lead">
-          {this.props.collection.description}
-        </div>
-      </div>
-    </div>
   }
 });
