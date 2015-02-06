1207e53ae4fcc370a61bd05966497de232dc8622
lachlanjc <lachlan.campbell@icloud.com>
2015-02-05 21:04:46 -0500

    Combine all the pushes

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -4,25 +4,14 @@ class AnalyticsController < ApplicationController
   def dashboard
     @recipes_count = Recipe.all.count
     @recipes_shared_count = Recipe.where(:shared => true).count
-    @users = User.all.order(created_at: :desc)
 
-    @users.each do |user|
-      if Recipe.where(:user_id => user.id).count == 1
-        @users_with_one.push(user)
-      end
-    end
-
-    @users.each do |user|
-      if Recipe.where(:user_id => user.id).count > 1
-        @users_with_many.push(user)
-      end
-    end
     @users_with_one, @users_with_many, @users_with_none = []
 
-    @users.each do |user|
-      if Recipe.where(:user_id => user.id).count == 0
-        @users_with_none.push(user)
-      end
+    User.all.order(created_at: :desc).each do |user|
+      @user_recipe_count = Recipe.where(:user_id => user.id).count
+      @users_with_none.push(user) if @recipes.count == 0
+      @users_with_one.push(user)  if @recipes.count == 1
+      @users_with_many.push(user) if @recipes.count > 1
     end
   end
 
