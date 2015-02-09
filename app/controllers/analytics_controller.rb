1b3d2238b0ae0cff8d8b08dc659a2358c164e2bf
Lachlan Campbell <lachlanjc@users.noreply.github.com>
2015-02-09 08:15:49 -0500

    Trying to fix admin dashboard

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -6,13 +6,13 @@ class AnalyticsController < ApplicationController
     @recipes_count = Recipe.all.count
     @recipes_shared_count = Recipe.where(:shared => true).count
 
-    @users_with_one = @users_with_many = @users_with_none = []
+    @users_with_none, @users_with_one, @users_with_many = []
 
-    User.all.order(created_at: :desc).each do |user|
-      @user_recipe_count = Recipe.where(:user_id => user.id).count
-      @users_with_none.push(user) if @user_recipe_count == 0
-      @users_with_one.push(user)  if @user_recipe_count == 1
-      @users_with_many.push(user) if @user_recipe_count > 1
+    User.all.each do |user|
+      user_recipe_count = Recipe.where(:user_id => user.id).count
+      @users_with_none.push(user) if user_recipe_count == 0
+      @users_with_one.push(user)  if user_recipe_count == 1
+      @users_with_many.push(user) if user_recipe_count > 1
     end
   end
 
