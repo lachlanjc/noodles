58c4a67e58de4647d4e801149dea6c24c0e8502a
lachlanjc <lachlan.campbell@icloud.com>
2015-02-09 22:09:22 -0500

    Close #43

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -6,16 +6,18 @@ class AnalyticsController < ApplicationController
     @recipes_count = Recipe.all.count
     @recipes_shared_count = Recipe.where(:shared => true).count
 
-    users_with_none = []
-    users_with_one = []
-    users_with_many = []
+    @users_with_none = []
+    @users_with_one = []
+    @users_with_many = []
 
     User.all.each do |user|
-      user_recipe_count = Recipe.where(:user_id => user.id).count
-      users_with_none.push(user) if user_recipe_count == 0
-      users_with_one.push(user)  if user_recipe_count == 1
-      users_with_many.push(user) if user_recipe_count > 1
+      @user_recipe_count = Recipe.where(:user_id => user.id).count
+      @users_with_none.push(user) if @user_recipe_count == 0
+      @users_with_one.push(user)  if @user_recipe_count == 1
+      @users_with_many.push(user) if @user_recipe_count > 1
     end
+
+    render :dashboard
   end
 
   def all_users
