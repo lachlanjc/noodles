963c59d06680c91e7755744e667ad9fba70fa2df
lachlanjc <lachlan.campbell@icloud.com>
2015-02-16 20:50:31 -0500

    Starting better SEO

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,13 @@ class PagesController < ApplicationController
     if user_signed_in?
       redirect_to recipes_path
     else
+      expires_in 6.months, :public => true
       render :home, layout: false
     end
   end
 
   def home_forced
+    expires_in 2.months, :public => true
     render :home, layout: false
   end
 
