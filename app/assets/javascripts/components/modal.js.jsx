7053672ac515eb8f5ada95a88aab6219f15fef86
lachlanjc <lachlan.campbell@icloud.com>
2015-02-21 00:19:50 -0500

    Fix ModalClose display issue, closes #52

--- app/assets/javascripts/components/modal.js.jsx
+++ app/assets/javascripts/components/modal.js.jsx
@@ -1,7 +1,6 @@
-// Actual modals are coming...
 var ModalClose = React.createClass({
   render: function() {
-    return <a className="modal-close float-right" href="#">
+    return <a className="modal-close right" href="#">
       <IconClose />
     </a>
   }
