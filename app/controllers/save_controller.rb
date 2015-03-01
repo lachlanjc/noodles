007f86f5ed47e6c2eb4d6d269b63344dfde1724b
lachlanjc <lachlan.campbell@icloud.com>
2015-03-01 16:03:51 -0500

    Say sorry about loser websites

--- app/controllers/save_controller.rb
+++ app/controllers/save_controller.rb
@@ -6,6 +6,11 @@ class SaveController < ApplicationController
       if user_signed_in?
         recipe_data = scrape_for_schema_data(params[:url])
         recipe_data["source"] = params[:url]
+        if recipe_data["title"].blank? && recipe_data["ingredients"].blank?
+          # If we're not getting anything, we need to say sorry
+          flash[:danger] = "Sorry, that website hasn't implemented the schema.org tags correctly."
+          redirect_to recipes_path
+        end
         create_recipe(recipe_data, params[:url], "We've saved #{recipe_data['title']} from #{find_host(params[:url])}.")
       else
         flash[:info] = "Hey! You'll need an account in Noodles."
