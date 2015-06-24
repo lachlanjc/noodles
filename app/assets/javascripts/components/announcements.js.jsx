1186f85561572b2a93f8a2927951b311a8243a15
lachlanjc <hello@lachlanjc.me>
2015-06-23 21:44:30 -0500

    A little React cleanup

--- app/assets/javascripts/components/announcements.js.jsx
+++ app/assets/javascripts/components/announcements.js.jsx
@@ -1,20 +1,26 @@
 var AnnouncementItem = React.createClass({
   render: function() {
-    return <div className="bg-white rounded shadow mb2 py2 px3">
-      <AnnouncementBody data={this.props.data} />
-    </div>
+    return (
+      <div className="bg-white rounded shadow mb2 py2 px3">
+        <AnnouncementBody data={this.props.data} />
+      </div>
+    );
   }
 });
+
 var AnnouncementBody = React.createClass({
   render: function() {
-    return <div>
-      <h1 className="mt1 mb0 inline-block">{this.props.data.title}</h1>
-      <a className="right grey-5 h5 mt2 caps bold" href={this.props.data.url}>Permalink</a>
-      <p className="grey-3">Published by Lachlan on {this.props.data.created_at}</p>
-      <div className="text" dangerouslySetInnerHTML={{ __html: this.props.data.body_rendered }}></div>
-    </div>
+    return (
+      <div>
+        <h1 className="mt1 mb0 inline-block">{this.props.data.title}</h1>
+        <a className="right grey-5 h5 mt2 caps bold" href={this.props.data.url}>Permalink</a>
+        <p className="grey-3">Published by Lachlan on {this.props.data.created_at}</p>
+        <div className="text" dangerouslySetInnerHTML={{ __html: this.props.data.body_rendered }}></div>
+      </div>
+    );
   }
 });
+
 var AnnouncementList = React.createClass({
   render: function() {
     return (
--- app/assets/javascripts/components/bio.js.jsx
+++ app/assets/javascripts/components/bio.js.jsx
@@ -1,24 +1,28 @@
 var BioLachlan = React.createClass({
   render: function() {
-    return <div className="bio mt2">
-      <div className="bio-photo inline-block left">
-        <img src="http://d1fmxjrxw87eps.cloudfront.net/me.jpg" className="circle shadow" />
+    return (
+      <div className="bio mt2">
+        <div className="bio-photo inline-block left">
+          <img src="http://d1fmxjrxw87eps.cloudfront.net/me.jpg" className="circle shadow" />
+        </div>
+        <div className="bio-info inline-block mt2 ml2">
+          <h2 className="inline-block m0">Lachlan Campbell</h2>
+          <a href="https://twitter.com/lachlanjc" className="h5 white bold inline-block px1 ml1 rounded bg-orange up-6">@lachlanjc</a>
+          <p>Design + hack + middle school. Founder of Noodles.</p>
+        </div>
       </div>
-      <div className="bio-info inline-block mt2 ml2">
-        <h2 className="inline-block m0">Lachlan Campbell</h2>
-        <a href="https://twitter.com/lachlanjc" className="h5 white bold inline-block px1 ml1 rounded bg-orange up-6">@lachlanjc</a>
-        <p>Design + hack + middle school. Founder of Noodles.</p>
-      </div>
-    </div>
+    );
   }
 });
 
 var BioTaran = React.createClass({
   render: function() {
-    return <div className="mt2">
-      <h2 className="inline-block m0">Taran Samarth</h2>
-      <a href="https://twitter.com/tarans22" className="h5 white bold inline-block px1 ml1 rounded bg-orange up-6">@tarans22</a>
-      <p>Co-founder of Noodles. Hacker, writer, and football geek.</p>
-    </div>
+    return (
+      <div className="mt2">
+        <h2 className="inline-block m0">Taran Samarth</h2>
+        <a href="https://twitter.com/tarans22" className="h5 white bold inline-block px1 ml1 rounded bg-orange up-6">@tarans22</a>
+        <p>Co-founder of Noodles. Hacker, writer, and football geek.</p>
+      </div>
+    );
   }
 });
