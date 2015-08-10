6e32cb1be8ad51d7149bb7fe279db037680077e3
Lachlan Campbell <hello@lachlanjc.me>
2015-08-09 23:06:21 -0400

    a lot of improvements to announcements

--- app/assets/javascripts/components/announcements.js.jsx
+++ app/assets/javascripts/components/announcements.js.jsx
@@ -1,36 +1,38 @@
-var AnnouncementItem = React.createClass({
-  render: function() {
+class AnnouncementItem extends React.Component {
+  render() {
     return (
       <div className="bg-white rounded shadow mb2 py2 px3">
         <AnnouncementBody data={this.props.data} />
       </div>
-    );
+    )
   }
-});
+}
 
-var AnnouncementBody = React.createClass({
-  render: function() {
+class AnnouncementBody extends React.Component {
+  render() {
     return (
       <div>
         <h1 className="mt1 mb0 inline-block">{this.props.data.title}</h1>
-        <a className="right grey-5 h5 mt2 caps bold" href={this.props.data.url}>Permalink</a>
-        <p className="grey-3">Published by Lachlan on {this.props.data.created_at}</p>
+        <p className="grey-3">
+          Published {this.props.data.created_at} · {""}
+          <a className="link-reset" href={"/announcements/" + this.props.data.id.toString()}>Permalink</a>
+        </p>
         <div className="text" dangerouslySetInnerHTML={{ __html: this.props.data.body_rendered }}></div>
       </div>
-    );
+    )
   }
-});
+}
 
-var AnnouncementList = React.createClass({
-  render: function() {
+class AnnouncementList extends React.Component {
+  render() {
     return (
-      <article className="sm-col-11 md-col-7 mx-auto">
+      <main className="sm-col-11 md-col-8 mx-auto py2">
         <h1>Noodles keeps getting better.</h1>
         <h2 className="grey-3 mt0 mb3">Check back here for the latest updates.</h2>
         {this.props.announcements.map(function(announcement) {
-           return <AnnouncementItem key={announcement.id} data={announcement}/>;
+           return <AnnouncementItem key={"announcement-" + announcement.id} data={announcement}/>;
         })}
-      </article>
-    );
+      </main>
+    )
   }
-});
+}
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -6,7 +6,7 @@ class Announcement < ActiveRecord::Base
   def as_json
     {
       title: title,
-      url: "http://www.getnoodl.es/announcements/" + id.to_s,
+      id: id.to_s,
       created_at: created_at.strftime("%B %e, %Y"),
       body_rendered: body_rendered
     }
