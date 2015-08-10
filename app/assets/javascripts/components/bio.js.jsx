5872e359f26566acb70841105659a026a23857e4
Lachlan Campbell <hello@lachlanjc.me>
2015-08-09 23:05:32 -0400

    fancy new ES6, JS-based bios

--- app/assets/javascripts/components/bio.js.jsx
+++ app/assets/javascripts/components/bio.js.jsx
@@ -1,23 +1,27 @@
-var BioLachlan = React.createClass({
-  render: function() {
+class Bio extends React.Component {
+  render() {
+    const lachlan = {
+      name: "Lachlan Campbell",
+      description: "Creator of Noodles, middle schooler, and intern at Highrise HQ.",
+      username: "lachlanjc"
+    }
+    const taran = {
+      name: "Taran Samarth",
+      description: "Co-founder of Noodles. Hacker, writer, and football geek.",
+      username: "tarans22"
+    }
+    let user = lachlan;
+    if (this.props.person == "tns") {
+      user = taran;
+    }
     return (
       <div className="py2">
-        <h3 className="m0">Lachlan Campbell</h3>
-        <p className="m0">Creator of Noodles, middle schooler, and intern at <a href="https://highrisehq.com/">Highrise</a>.</p>
-        <a href="https://twitter.com/lachlanjc" className="h5 white bold inline-block px1 rounded bg-orange">@lachlanjc</a>
+        <h3 className="m0">{user.name}</h3>
+        <p className="m0">{user.description}</p>
+        <a href={"https://twitter.com/" + user.username} className="h5 white bold inline-block px1 rounded bg-orange">
+          @{user.username}
+        </a>
       </div>
     )
   }
-});
-
-var BioTaran = React.createClass({
-  render: function() {
-    return (
-      <div className="py2">
-        <h3 className="m0">Taran Samarth</h3>
-        <p className="m0">Co-founder of Noodles. Hacker, writer, and football geek.</p>
-        <a href="https://twitter.com/tarans22" className="h5 white bold inline-block px1 rounded bg-orange">@tarans22</a>
-      </div>
-    )
-  }
-});
+}
