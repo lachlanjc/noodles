791e2be79f3dfe3efc1112a6dd0c55c4ab9ddc66
Lachlan Campbell <hello@lachlanjc.me>
2015-08-17 11:54:58 -0400

    notes for epicurious scraper

--- app/helpers/scrapers/epicurious.rb
+++ app/helpers/scrapers/epicurious.rb
@@ -9,16 +9,19 @@ class EpicuriousScraper
       path url_path
 
       title css: "#headline h1"
-      description css: "#recipe_summary #recipeIntroText p"
+      description css: "#recipe_summary #recipeIntroText p:not(#chefNotes)"
       ingredients({ css: ".ingredientsList .ingredient" }, :list)
       instructions({ css: ".instructions > p" }, :list)
       # source css: ".instructions #additional-info .recipe-source-info"
       serves css: "#recipe_summary .summary_data span"
+
+      notes css: "#preparation #chefNotes"
     end
     recipe["instructions"].each do |line|
       # Removes their step numbers
       line.sub!(/(\d+)\.\s/, "")
     end
-    return recipe
+    recipe["notes"].to_s.sub!(/Cooks\' Note\n/, "").strip!
+    recipe
   end
 end
