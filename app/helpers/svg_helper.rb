e747b6a1fe02a4ae14e3b9c08d611897325ca1cd
Lachlan Campbell <hello@lachlanjc.me>
2015-08-21 08:45:02 -0400

    rewrite inline_svg helper
    uses Nokogiri and supports the style attribute.

--- app/helpers/svg_helper.rb
+++ app/helpers/svg_helper.rb
@@ -1,10 +1,19 @@
 module SvgHelper
-  def inline_svg(path, options = {})
-    file = File.open("app/assets/images/#{path}", "rb")
-    svg = raw file.read
+  def inline_svg(filename, options = {})
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
     if options[:class].present?
-      svg["class"] = options[:class]
+      svg['class'] = options[:class]
     end
-    return svg
+    if options[:style].present?
+      svg['style'] = options[:style]
+    end
+    doc.to_html.html_safe
+  end
+
+  def twitter_icon
+    inline_svg("social-twitter.svg", { style: "fill: #00aced" })
+  end
   end
 end
