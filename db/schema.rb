c4631138983d3c8fe6f15029bdce4710ff1b2e6a
Lachlan Campbell <hello@lachlanjc.me>
2015-08-26 14:17:29 -0400

    add author

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204220031) do
+ActiveRecord::Schema.define(version: 20150826181127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150204220031) do
     t.string   "serves"
     t.string   "shared_id"
     t.text     "collections",           default: [], array: true
+    t.string   "author"
   end
 
   create_table "users", force: true do |t|
