d5f3fa1db98baf71dd136deb423a831d78070066
Lachlan Campbell <hello@lachlanjc.me>
2015-08-31 00:16:14 -0400

    favorites blankslate

--- app/assets/javascripts/components/recipes_home.js.jsx
+++ app/assets/javascripts/components/recipes_home.js.jsx
@@ -42,6 +42,17 @@ class RecipesHome extends React.Component {
     return classes;
   }
 
+  renderFavoritesBlankSlate() {
+    return (
+      <BlankSlate>
+        <h3 className='mt0'>No favorites yet.</h3>
+        <p>Favorites are an easy way to quickly bookmark recipes. They're marked with a star.</p>
+        <p>To favorite a recipe, open the recipe and click the star to the right of the recipe's title.</p>
+        <a onClick={e => this.findAll(e)} className='btn bg-blue'>Back to all recipes</a>
+      </BlankSlate>
+    )
+  }
+
   render() {
     const recipes = this.state.recipesCurrent;
     const randomUrl = (recipes.length > 0) ? '/recipes/' + recipes[Math.floor(Math.random() * recipes.length)].id.toString() : null;
@@ -57,6 +68,7 @@ class RecipesHome extends React.Component {
           </section>
         </header>
         <RecipeList recipesCore={this.state.recipesCurrent} createFromSearch={true} />
+        {this.state.view === 'fav' && this.state.recipesCurrent.length == 0 ? this.renderFavoritesBlankSlate() : null}
       </main>
     )
   }
