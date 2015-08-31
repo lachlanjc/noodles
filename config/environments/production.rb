15f1ed71b2291876d81c144bc5d8ab702cbe670f
Lachlan Campbell <hello@lachlanjc.me>
2015-08-31 09:07:17 -0400

    serve_static_files is the new serve_static_assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
