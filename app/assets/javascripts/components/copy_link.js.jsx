b0465fdb8e1a530a03a952dd012cf8b69aec1ba0
Lachlan Campbell <hello@lachlanjc.me>
2015-09-03 08:07:07 -0400

    es6 copylink

--- app/assets/javascripts/components/copy_link.js.jsx
+++ app/assets/javascripts/components/copy_link.js.jsx
@@ -1,36 +1,40 @@
-var CopyLink = React.createClass({
-  getInitialState: function() {
-    return { label: "Copy" }
-  },
+class CopyLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false
+    };
+  }
 
-  render: function() {
+  render() {
+    const label = this.state.copied ? 'Copied!' : 'Copy';
     return (
-      <div className="h5">
-        <input ref="text"
-            type="text"
-            className="text-input"
-            style={{display: "inline-block"}}
-            value={this.props.url}
-            onChange={function() {}} />
-          <button ref="copy" className="btn bg-green ml2" type="button">{this.state.label}</button>
+      <div className='h5'>
+        <input ref='text'
+              type='text'
+              className='text-input'
+              style={{display: 'inline-block'}}
+              value={this.props.url}
+              onChange={function() {}} />
+          <button ref='copy' className='btn bg-green ml2' type='button'>{label}</button>
       </div>
-    );
-  },
+    )
+  }
 
-  componentDidMount: function() {
-    var self = this;
-    var client = new ZeroClipboard(this.refs.copy.getDOMNode());
-    client.on("ready", function(event) {
-      client.on("copy", function(event) {
-        event.clipboardData.setData("text/plain", self.props.url)
+  componentDidMount() {
+    const self = this;
+    const client = new ZeroClipboard(this.refs.copy.getDOMNode());
+    client.on('ready', function(e) {
+      client.on('copy', function(e) {
+        e.clipboardData.setData('text/plain', self.props.url)
       });
 
-      client.on("aftercopy", function(e) {
-        self.setState({label: "Copied!"});
+      client.on('aftercopy', function(e) {
+        self.setState({copied: true});
         setTimeout(function() {
-          self.setState({label: "Copy"})
+          self.setState({copied: false})
         }, 1000);
       });
     });
   }
-});
+}
