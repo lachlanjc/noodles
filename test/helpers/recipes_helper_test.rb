157241bd63725eed1a80d7466374db6ef647f42d
Lachlan Campbell <hello@lachlanjc.me>
2015-09-06 01:31:55 -0400

    test recipe helpers

--- test/helpers/recipes_helper_test.rb
+++ test/helpers/recipes_helper_test.rb
@@ -1,4 +1,25 @@
 require 'test_helper'
 
 class RecipesHelperTest < ActionView::TestCase
+  include RecipesHelper
+
+  test 'should generate shared id' do
+    assert_equal 'w0aLgD', generate_shared_id(1)
+  end
+
+  test 'should generate recipe share link' do
+    assert_equal 'http://test.host/s/goodcheese', shared_url(recipes(:one))
+  end
+
+  test 'should test recipe is from web' do
+    assert is_from_web('http://www.getnoodl.es/s/6ww5W0L')
+  end
+
+  test 'processed ingredients should work' do
+    assert_not_empty ingredient_processed('hello')
+  end
+
+  test 'plain text should work' do
+    assert_not_empty plain_text_from_markdown('**hello**')
+  end
 end
