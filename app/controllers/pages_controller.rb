63209563166f491e1874245ef3071294253e45f0
Lachlan Campbell <hello@lachlanjc.me>
2015-09-10 13:11:39 -0400

    remove homepage caching
    I had added this trying to make the homepage faster, but it meant that
    flashes/auth state were being cached, causing serious login issues.
    Getting rid of it all.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,9 @@
 class PagesController < ApplicationController
   def home
-    expires_in 6.months, :public => true
     render :home, layout: false
   end
 
   def home_forced
-    expires_in 2.months, :public => true
     render :home, layout: false
   end
 
