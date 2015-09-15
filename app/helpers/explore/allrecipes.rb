0d891d689b33aea4ea5973334f00d280d5f27356
Lachlan Campbell <hello@lachlanjc.me>
2015-09-15 08:09:56 -0400

    some of it's working...

--- app/helpers/explore/allrecipes.rb
+++ app/helpers/explore/allrecipes.rb
@@ -7,8 +7,19 @@ class AllrecipesSearchScraper
 
     scraper = Mechanize.new
     scraper.history_added = Proc.new { sleep 0.4 }
-    base_url = 'http://allrecipes.com'
-    raw_data = []
+
     results = []
+    raw_results = scraper.get('http://allrecipes.com/search/results/?sort=re&wt=' + q).search('section.grid article:not(.dfp_container)')
+    raw_results.each do |item|
+      result = {}
+      result['url'] = 'http://allrecipes.com/recipe/' + item.search('ar-save-item').attr('data-id').to_s
+      result['title'] = item.search('h3').text.strip
+      result['description'] = item.search('.rec-card__description').text.strip
+      results.push(result)
+    end
+    results.delete_if do |item|
+      item['title'].blank?
+    end
+    results
   end
 end
