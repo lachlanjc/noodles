be4a114602bc21e20504eabb28704aa92b296766
Lachlan Campbell <hello@lachlanjc.me>
2015-09-26 12:05:50 -0400

    work on allrecipes for explore

--- app/helpers/explore/allrecipes.rb
+++ app/helpers/explore/allrecipes.rb
@@ -12,13 +12,14 @@ class AllrecipesSearchScraper
     raw_results = scraper.get('http://allrecipes.com/search/results/?sort=re&wt=' + q).search('section.grid article:not(.dfp_container)')
     raw_results.each do |item|
       result = {}
-      result['url'] = 'http://allrecipes.com/recipe/' + item.search('ar-save-item').attr('data-id').to_s
+      # result['url'] = 'http://allrecipes.com/recipe/'
+      result['url'] = item.search('a').attr('href').to_s
       result['title'] = item.search('h3').text.strip
       result['description'] = item.search('.rec-card__description').text.strip
       results.push(result)
     end
     results.delete_if do |item|
-      item['title'].blank?
+      item['url'].blank? || item['title'].blank?
     end
     results
   end
