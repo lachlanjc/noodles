04a72a0757497515a50515f02e85a307d18bc4a7
Lachlan Campbell <hello@lachlanjc.me>
2015-09-27 21:07:12 -0400

    fix up privacy stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-  def please_sign_in
-    unless user_signed_in?
-      flash[:red] = "Please log in to an account first!"
-      redirect_to root_url
-    end
-  end
-
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :first_name
