13614b173f8f433d927c89b966169b61040dfef4
Lachlan Campbell <hello@lachlanjc.me>
2015-09-28 08:15:15 -0400

    add tests for allrecipes results and preview

--- test/controllers/explore_controller_test.rb
+++ test/controllers/explore_controller_test.rb
@@ -21,6 +21,12 @@ class ExploreControllerTest < ActionController::TestCase
     assert_not_nil assigns(:results)
   end
 
+  test 'should get allrecipes results' do
+    get :results, { src: 'allrecipes', q: 'pancakes' }
+    assert_response :success
+    assert_not_nil assigns(:results)
+  end
+
   test 'should get nyt preview' do
     get :preview, { url: 'http://cooking.nytimes.com/recipes/1012732-bacon-lettuce-and-plum-sandwiches' }
     assert_response :success
@@ -29,6 +35,12 @@ class ExploreControllerTest < ActionController::TestCase
 
   test 'should get epicurious preview' do
     get :preview, { url: 'http://www.epicurious.com/recipes/food/views/sheet-pan-grilled-cheese-56390006' }
+    assert_response :success
+    assert_not_nil assigns(:recipe)
+  end
+
+  test 'should get allrecipes preview' do
+    get :preview, { url: 'http://allrecipes.com/recipe/206086/rice-pancakes/' }
     assert_response :success
     assert_not_nil assigns(:recipe)
   end
