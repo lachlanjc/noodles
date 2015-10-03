ab834b9fc2cd424b2538b70d16c96677259c19f0
Lachlan Campbell <hello@lachlanjc.me>
2015-10-03 00:18:46 -0400

    remove blog "permalink" stuff

--- app/assets/javascripts/components/announcements.js.jsx
+++ app/assets/javascripts/components/announcements.js.jsx
@@ -1,7 +1,7 @@
 class AnnouncementItem extends React.Component {
   render() {
     return (
-      <div className="bg-white rounded shadow mb2 py2 px3">
+      <div className='bg-white rounded shadow mb2 py2 px3'>
         <AnnouncementBody data={this.props.data} />
       </div>
     )
@@ -12,12 +12,15 @@ class AnnouncementBody extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="mt1 mb0 inline-block">{this.props.data.title}</h1>
-        <p className="grey-3">
-          Published {this.props.data.created_at} · {""}
-          <a className="link-reset" href={"/announcements/" + this.props.data.id.toString()}>Permalink</a>
+        <h1 className='mt0 mb0 inline-block'>
+          <a href={'/announcements/' + this.props.data.id.toString()}>
+            {this.props.data.title}
+          </a>
+        </h1>
+        <p className='grey-3'>
+          Published on {this.props.data.created_at}
         </p>
-        <div className="text" dangerouslySetInnerHTML={{ __html: this.props.data.body_rendered }}></div>
+        <div className='text' dangerouslySetInnerHTML={{ __html: this.props.data.body_rendered }}></div>
       </div>
     )
   }
@@ -26,11 +29,11 @@ class AnnouncementBody extends React.Component {
 class AnnouncementList extends React.Component {
   render() {
     return (
-      <main className="sm-col-11 md-col-8 mx-auto py2">
-        <h1>Noodles keeps getting better.</h1>
-        <h2 className="grey-3 mt0 mb3">Check back here for the latest updates.</h2>
+      <main className='md-col-8 mx-auto p2'>
+        <h1 className='mb0'>Noodles keeps getting better.</h1>
+        <h2 className='grey-3 mt1 mb3 regular'>Check back here for the latest updates.</h2>
         {this.props.announcements.map(function(announcement) {
-           return <AnnouncementItem key={"announcement-" + announcement.id} data={announcement}/>;
+           return <AnnouncementItem key={announcement.id} data={announcement} />;
         })}
       </main>
     )
