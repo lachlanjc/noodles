a550e9daae90602f5a47746680dbec7a12e4824d
Lachlan Campbell <hello@lachlanjc.me>
2015-10-03 23:41:53 -0400

    collections - match up those gridlines

--- app/assets/javascripts/components/collection.js.jsx
+++ app/assets/javascripts/components/collection.js.jsx
@@ -23,9 +23,7 @@ class CollectionPage extends React.Component {
       <main>
         <CollectionHeader edit={this.props.edit} pub={pub} coll={this.state.coll} />
         {!_.isEmpty(this.state.coll.recipes) ?
-          <article className='md-col-8 mx-auto mt0'>
-            <RecipeList recipesCore={this.state.coll.recipes} linkType={pub ? 'public' : 'private'} />
-          </article>
+          <RecipeList recipesCore={this.state.coll.recipes} linkType={pub ? 'public' : 'private'} className='md-col-8 px2' />
         : this.renderNoRecipes()}
       </main>
     )
@@ -52,11 +50,7 @@ class CollectionPage extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        {!_.isEmpty(this.state.coll) ? this.renderPage() : this.renderLoading()}
-      </div>
-    )
+    return !_.isEmpty(this.state.coll) ? this.renderPage() : this.renderLoading();
   }
 }
 
@@ -70,18 +64,18 @@ class CollectionHeader extends React.Component {
 
     let rootClass = 'col-12 bs-bb flex flex-center';
     let rootStyle = {};
-    let actionsClass = 'caps h4 mt2 print-hide';
+    let actionsClass = 'caps h4 print-hide mt2';
     if (!_.isEmpty(coll.photo)) {
-      rootClass += ' image-header relative inline-with-nav p3 bg-cover bg-center bg-no-repeat';
+      rootClass += ' image-header relative inline-with-nav py3 bg-cover bg-center bg-no-repeat';
       rootStyle.backgroundImage = `url(${coll.photo_url})`;
-      actionsClass += ' block white mb2';
+      actionsClass += ' mb2 block white';
     } else {
       rootClass += ' center grey-4 py2';
     }
 
     return (
       <header className={rootClass} style={{backgroundImage: `url(${coll.photo})`}}>
-        <div className='sm-col-12 md-col-8 mx-auto mw7'>
+        <div className='sm-col-12 md-col-8 mx-auto px2 mw7'>
           <h1 className='coll-name m0 h0'>{coll.name}</h1>
           {!_.isEmpty(coll.description) ? <p className='h3 mt1 mb0 coll-desc'>{coll.description}</p> : null}
           {this.props.pub ?
