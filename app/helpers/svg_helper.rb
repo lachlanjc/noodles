94f34f999937a8708158a5a41b1f0e125e97260c
Lachlan Campbell <hello@lachlanjc.me>
2015-11-06 17:28:13 -0500

    beter svg options processing

--- app/helpers/svg_helper.rb
+++ app/helpers/svg_helper.rb
@@ -3,11 +3,12 @@ module SvgHelper
     file = File.read(Rails.root.join('app', 'assets', 'images', filename))
     doc = Nokogiri::HTML::DocumentFragment.parse file
     svg = doc.at_css 'svg'
-    options.each { |key, value| svg["#{key}"] = value }
     if options[:size]
-      svg['width'], svg['height'] = options[:size]
-      svg.delete 'size'
+      options[:width] = options[:size]
+      options[:height] = options[:size]
+      options.delete :size
     end
+    options.each { |key, value| svg["#{key}"] = value }
     doc.to_html.html_safe
   end
 
