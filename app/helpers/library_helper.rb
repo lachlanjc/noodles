96cb480c45bafc98183a1816f10356a3f3c1f3b3
Lachlan Campbell <hello@lachlanjc.me>
2015-11-08 02:00:38 -0500

    a really nice library

--- app/helpers/library_helper.rb
+++ app/helpers/library_helper.rb
@@ -7,7 +7,13 @@ module LibraryHelper
     !me_owns_page?
   end
 
-  def page_title(page = @page)
-    @page.name.present? ? @page.name.to_s.html_safe : 'New page'
+  def page_title(page = @page, text = 'Untitled')
+    page.name.present? ? page.name.to_s : text.to_s
+  end
+
+  def page_preview_text(text)
+    text = text.to_s.gsub('<li>', '• ').gsub('</li>', '<br>')
+    text = Sanitize.fragment text, elements: %w(strong del br)
+    text.html_safe
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,21 @@
 class Page < ActiveRecord::Base
+  include LibraryHelper
+
   belongs_to :user
 
-  validates :name, presence: true, length: { maximum: 254 }
+  validates :name, presence: true, length: { maximum: 255 }
   validates :user_id, presence: true
+
+  before_save :clean_page_name!
+  before_save :generate_page_preview_text!
+
+  protected
+
+  def clean_page_name!
+    self.name.to_s.squish!
+  end
+
+  def generate_page_preview_text!
+    self.content_raw = page_preview_text(self.content)
+  end
 end
