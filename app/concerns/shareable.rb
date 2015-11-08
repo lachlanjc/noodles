e44e8b0b06b831e75d1f8ac64e94d0817ca543a4
Lachlan Campbell <hello@lachlanjc.me>
2015-11-08 15:25:16 -0500

    fix shareable

--- app/concerns/shareable.rb
+++ app/concerns/shareable.rb
@@ -2,14 +2,18 @@ module Shareable
   extend ActiveSupport::Concern
 
   included do
+    before_validation :generate_shared_id!, on: :create
+    before_validation :check_shared_id
     validates :shared_id, presence: true, uniqueness: true
-    before_validation :generate_shared_id, on: :create
-    after_validation :generate_shared_id, on: :create, unless: Proc.new { |recipe| recipe.errors.empty? }
   end
 
-  private
-
-  def generate_shared_id
+  def generate_shared_id!
     self.shared_id = rand(32**8).to_s(32)
+  end
+
+  protected
+
+  def check_shared_id
+    self.generate_shared_id! if self.shared_id.blank?
   end
 end
