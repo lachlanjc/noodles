64e1203a3a492c62bf1f3182a67b250fdb61bd0e
Lachlan Campbell <hello@lachlanjc.me>
2015-11-14 23:51:49 -0500

    add fill option to inline svgs

--- app/helpers/svg_helper.rb
+++ app/helpers/svg_helper.rb
@@ -8,31 +8,35 @@ module SvgHelper
       options[:height] = options[:size]
       options.delete :size
     end
+    if options[:fill]
+      options[:style] += "fill: ##{options[:fill].to_s};"
+      options.delete :fill
+    end
     options.each { |key, value| svg["#{key}"] = value }
     doc.to_html.html_safe
   end
 
   def twitter_icon
-    inline_svg 'social-twitter.svg', style: 'fill: #00aced'
+    inline_svg 'social-twitter.svg', fill: '00aced'
   end
 
   def facebook_icon
-    inline_svg 'social-facebook.svg', style: 'fill: #3b5998'
+    inline_svg 'social-facebook.svg', fill: '3b5998'
   end
 
   def pinterest_icon
-    inline_svg 'social-pinterest.svg', style: 'fill: #cb2027'
+    inline_svg 'social-pinterest.svg', fill: 'cb2027'
   end
 
   def buffer_icon
-    inline_svg 'social-buffer.svg', style: 'fill: #555'
+    inline_svg 'social-buffer.svg', fill: '555'
   end
 
   def googleplus_icon
-    inline_svg 'social-google-plus.svg', style: 'fill: #dc4e41'
+    inline_svg 'social-google-plus.svg', fill: 'dc4e41'
   end
 
   def email_icon
-    inline_svg 'social-email.svg', style: 'fill: #0092ff'
+    inline_svg 'social-email.svg', fill: '0092ff'
   end
 end
