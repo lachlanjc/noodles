42ae524141cd588aeaea8cd249a0dfd2337d7aa5
Lachlan Campbell <lachlanjc@users.noreply.github.com>
2016-02-11 20:13:46 -0500

    Fix whitespace on RegistrationsController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
-    def after_sign_up_path_for(resource)
-      params[:explore] ? explore_path : onboarding_path
-    end
+  
+  def after_sign_up_path_for(resource)
+    params[:explore] ? explore_path : onboarding_path
+  end
 
-    def update_resource(resource, params)
-      resource.update_without_password(params)
-    end
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
