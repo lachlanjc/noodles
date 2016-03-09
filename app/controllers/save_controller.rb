2ede9132c98b4b78273f6fdcddac7ebd1941ff62
Lachlan Campbell <atlachlanjc@gmail.com>
2016-03-09 12:19:52 -0500

    Simplify SaveController

--- app/controllers/save_controller.rb
+++ app/controllers/save_controller.rb
@@ -3,19 +3,15 @@ class SaveController < ApplicationController
   include ScrapingHelper
 
   before_filter :validate_url
+  before_filter :redirect_guests
 
   def save
-    if user_signed_in?
-      recipe_data = master_scrape(params[:url])
-      if recipe_data == false || recipe_data['title'].blank?
-        flash[:red] = 'Sorry — that site isn\'t working because it doesn\'t use standard markup for the recipe.'
-        go_back
-      else
-        create_recipe(recipe_data, params[:url], "Awesome! We've saved #{recipe_data['title']} from #{find_domain(params[:url]).to_s.humanize}.")
-      end
+    recipe_data = master_scrape(params[:url])
+    if recipe_data == false || recipe_data['title'].blank?
+      flash[:red] = 'Sorry — that site isn\'t working because it doesn\'t use standard markup for the recipe.'
+      go_back
     else
-      flash[:blue] = 'Hey there! Sign up for Noodles below to save that awesome recipe.'
-      redirect_to root_url
+      create_recipe(recipe_data, params[:url], "Awesome! We've saved #{recipe_data['title']} from #{find_domain(params[:url]).to_s.humanize}.")
     end
   end
 
@@ -23,11 +19,19 @@ class SaveController < ApplicationController
 
   def url_is_valid?(u = params[:url])
     url = URI.parse(u) rescue false
-    url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
+    url.is_a?(URI::HTTP) || url.is_a?(URI::HTTPS)
   end
 
   def validate_url
-    valid = url_is_valid?
-    render text: 'You didn\'t provide a valid recipe link.', status: 422 unless valid
+    unless url_is_valid?
+      flash[:red] = 'You didn\'t provide a valid recipe link.'
+      go_back
+    end
+  end
+
+  def redirect_guests
+    return if user_signed_in?
+    flash[:blue] = 'Hey there! Sign up for Noodles (below) to save that awesome recipe.'
+    redirect_to root_url
   end
 end
