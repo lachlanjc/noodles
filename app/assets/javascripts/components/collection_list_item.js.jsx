3f07f018ccf7ea75d48556529c182aa31940097d
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-03-15 19:27:36 -0400

    Pull out CollectionListItem

---
+++ app/assets/javascripts/components/collection_list_item.js.jsx
@@ -0,0 +1,26 @@
+class CollectionListItem extends React.Component {
+  render() {
+    const coll = this.props.coll
+    let imgClass, img
+    if (!_.isEmpty(coll.photo_url)) {
+      imgClass = 'image-header bg-center bg-no-repeat bg-cover'
+      img = `url(${coll.photo_url.toString()})`
+    } else {
+      imgClass = 'bg-white rounded shadow pam'
+    }
+
+    return (
+      <a href={coll.url} style={{minHeight: '15vh', backgroundImage: img}}
+         className={`flex fc fac fjc rounded shadow pal mbm bg-white rounded shadow ${imgClass}`}>
+        <h2 className='coll__name f1 mw7 mbm'>{coll.name}</h2>
+        {!_.isEmpty(coll.description) ?
+          <h3 className='coll__desc lead man normal mw7'>{coll.description}</h3>
+        : null}
+      </a>
+    )
+  }
+}
+
+CollectionListItem.propTypes = {
+  coll: React.PropTypes.object.isRequired
+}
