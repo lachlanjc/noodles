9760e542d178415243578faff560097662a92195
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-04-04 17:29:29 -0400

    blazing analytics 🔥🔥

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,9 +1,30 @@
 module AnalyticsHelper
+  include ApplicationHelper
+
   def user_recipes_count(user = current_user)
     user.recipes.count
   end
 
+  def heap_recipe_analytics
+    "'recipes_count': #{user_recipes_count}," if simple_controller == 'recipes'
+  end
+
   def user_collections_count(user = current_user)
     user.collections.count
+  end
+
+  def heap_coll_analytics
+    "'collections_count': #{user_collections_count}," if simple_controller == 'collections'
+  end
+
+  def intercom_custom_data
+    data = {}
+    if simple_controller == 'devise/registrations'
+      data[:referring_url] = current_user.referring_url
+      data[:landing_url] = current_user.landing_url
+    end
+    data[:recipes_count] = user_recipes_count if simple_controller == 'recipes'
+    data[:collections_count] = user_collections_count if simple_controller == 'collections'
+    data
   end
 end
