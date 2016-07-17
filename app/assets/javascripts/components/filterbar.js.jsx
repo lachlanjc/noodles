ae5f7c557ac74c0d4bfda967d645bcb98eb515a3
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-07-17 16:46:20 -0400

    stateless FilterBar(Item)

--- app/assets/javascripts/components/filterbar.js.jsx
+++ app/assets/javascripts/components/filterbar.js.jsx
@@ -1,32 +1,27 @@
-class FilterBar extends React.Component {
-  render() {
-    return (
-      <section role='menubar' className='filterbar tc' {...this.props}>
-        {this.props.children}
-      </section>
-    )
-  }
-}
 
-class FilterBarItem extends React.Component {
-  render() {
-    let classes = `filterbar__item ${this.props.className} dib phm pointer `
-    if (this.props.active === this.props.view) {
-      classes += 'bg-orange white white-hover b'
-    } else {
-      classes += 'grey-1'
-    }
+const FilterBar = ({ ...props }) => (
+  <section role='menubar' className='filterbar tc' {...props} />
+)
 
-    return (
-      <a role='menuitem' className={classes} {...this.props}>
-        {this.props.name}
-      </a>
-    )
+const FilterBarItem = ({ active, view, className, ...props }) => {
+  let cx
+  if (_.isEqual(active, view)) {
+    cx = 'bg-orange white white-hover b '
+  } else {
+    cx = 'grey-1 '
   }
+  cx = `filterbar__item dib phm pointer ${cx} ${className}`
+
+  return (
+    <a
+      role='menuitem'
+      className={cx}
+      {...props} />
+  )
 }
 
 FilterBarItem.propTypes = {
   active: React.PropTypes.string.isRequired,
   view: React.PropTypes.string.isRequired,
-  name: React.PropTypes.string.isRequired
+  className: React.PropTypes.string,
 }
