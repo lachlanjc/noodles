f9b1de8932afb0946e701cbf937f28ff9dd52ca4
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-09-06 13:26:02 -0400

    more protips

--- app/assets/javascripts/components/protip.js.jsx
+++ app/assets/javascripts/components/protip.js.jsx
@@ -1,7 +1,12 @@
 
 const tips = [
-  <span>Create a new recipe super quickly by searching with its title.</span>,
-  <span>Not sure which recipe to cook right now? Click <strong>Random</strong> at the top.</span>
+  'Create a new recipe super quickly by searching with its title.',
+  'Find a random recipe with the die icon next to search.',
+  'Use Cook Mode to check off ingredients as you cook.',
+  'Mark recipes as favorites with the star for quick access.',
+  'Save recipes from the web with the Clip icon in the right corner.',
+  'Share a recipe with anyone from the menu on the recipe page.',
+  'Download a PDF of a recipe from the menu on the recipe page.',
 ]
 
 const ProTip = () => {
@@ -9,12 +14,12 @@ const ProTip = () => {
   return (
     <footer className='tc'>
       <Icon
-        icon='protip'
+        icon='bolt'
         className='dib fill-grey-3 mrs relative'
         style={{ top: 6 }}
       />
       <strong
-        style={{ marginRight: 8 }}
+        style={{ marginRight: 4 }}
         children='ProTip!'
       />
       {tip}
