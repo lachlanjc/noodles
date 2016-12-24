f619274f489765653b286d2e670e169e85bd84ca
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-12-23 23:57:54 -0500

    clean up devise helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,8 +5,8 @@ module DeviseHelper
 
     messages = user.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
-    <div class="rounded mt2 mb2 phm mx-auto" style="border: 2px solid #ff0013; padding-bottom: 1.5rem;">
-      <h2 class="red mt3 tc">Those Noodles are a bit tangled.</h2>
+    <div class="rounded mvm phm mx-auto" style="border: 2px solid #ff0013; padding-bottom: 1.5rem;">
+      <h2 class="red mtn f3">Those Noodles are a bit tangled.</h2>
       <ul class="list-reset pan text tc">#{messages}</ul>
     </div>
     HTML
@@ -15,10 +15,6 @@ module DeviseHelper
   end
 
   def devise_error_messages?
-    resource.errors.empty? ? false : true
-  end
-
-  def nobody_signed_in?
-    !user_signed_in?
+    resource.errors.present?
   end
 end
