6d0a39add5f452bb035f9afb0af6c307287e3868
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-02-26 20:29:41 -0500

    create general job to ping me

---
+++ app/jobs/email_me_job.rb
@@ -0,0 +1,30 @@
+class EmailMeJob < ApplicationJob
+  include TextHelper
+
+  def perform(data)
+    Aws::SES::Client.new.send_email({
+      destination: {
+        to_addresses: [
+          'lachlan@getnoodl.es'
+        ],
+      },
+      message: {
+        body: {
+          html: {
+            charset: 'UTF-8',
+            data: markdown(data[:body]),
+          },
+          text: {
+            charset: 'UTF-8',
+            data: data[:body],
+          },
+        },
+        subject: {
+          charset: 'UTF-8',
+          data: data[:subject],
+        },
+      },
+      source: 'Noodles <app@getnoodl.es>'
+    })
+  end
+end
