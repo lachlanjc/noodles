987c4ca7a0cf15ece7d9d75b35b697f6410c8f3b
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-11-23 14:39:38 -0500

    Add Recently Cooked

---
+++ app/javascript/components/RecentlyCooked.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import SidebarModule, {
+  SidebarModuleHeading,
+  SidebarModuleList,
+  SidebarModuleListItem
+} from './SidebarModule'
+
+const findRecents = recipes =>
+  _.slice(_.sortBy(recipes, 'last_cooked_at'), 0, 5)
+
+const RecentlyCooked = ({ recipes, ...props }) => (
+  <SidebarModule {...props}>
+    <SidebarModuleHeading>
+      Recently cooked
+    </SidebarModuleHeading>
+    <SidebarModuleList>
+      {_.map(findRecents(recipes), recipe => (
+        <SidebarModuleListItem
+          name={recipe.title}
+          path={recipe.path}
+          key={recipe.path}
+        />
+      ))}
+    </SidebarModuleList>
+  </SidebarModule>
+)
+
+RecentlyCooked.propTypes = {
+  recipes: PropTypes.array.isRequired
+}
+
+export default RecentlyCooked
--- app/javascript/components/RecipesHomeSidebar.js
+++ app/javascript/components/RecipesHomeSidebar.js
@@ -2,18 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Button from './Button'
-import CollectionsMini from './CollectionsMini'
-import ModalLink from './ModalLink'
 import Spacer from './Spacer'
+import ModalLink from './ModalLink'
+import CollectionsMini from './CollectionsMini'
+import RecentlyCooked from './RecentlyCooked'
 
 const RecipesHomeSidebar = ({
+  recipes = [],
   collections = [],
   hideCollections = false,
   ...props
 }) => (
   <div className="dn-p">
     <Button primary color="blue" href="/recipes/new" children="New recipe" />
-    <Spacer x={12} y={16} className="dib db-ns" />
+    <Spacer x={16} y={16} className="dib db-ns" />
     <ModalLink
       primary
       is="btn"
@@ -21,19 +23,21 @@ const RecipesHomeSidebar = ({
       color="purple"
       children="Import"
     />
-    <Spacer x={12} y={16} className="dib db-ns" />
+    <Spacer x={16} y={16} className="dib db-ns" />
     <Button
       primary
       color="orange"
       href="/explore"
       children="Explore"
-      className="mtm mtn-ns"
+      className="mvm mtn-ns"
     />
     <CollectionsMini collections={collections} hidden={hideCollections} />
+    <RecentlyCooked recipes={recipes} hidden={_.size(recipes) < 8} />
   </div>
 )
 
 RecipesHomeSidebar.propTypes = {
+  recipes: PropTypes.array,
   collections: PropTypes.array,
   hideCollections: PropTypes.bool
 }
