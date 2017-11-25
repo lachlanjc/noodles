b2d64064a82a9f3e6a250a498ba4989e74daf70a
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-11-25 00:26:12 -0500

    Add phone to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_171_124_044_323) do
+ActiveRecord::Schema.define(version: 20_171_125_051_745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20_171_124_044_323) do
     t.string 'stripe_customer'
     t.string 'stripe_subscription'
     t.datetime 'subscribed_at'
+    t.string 'phone'
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
