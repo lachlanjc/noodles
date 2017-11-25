3b2be82426f47832669dd4f69dd89b68f29756e5
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-11-25 03:20:01 -0500

    Find SMS users

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
 class SmsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def groceries
     @client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
-    if ['groceries', 'grocery list', 'list'].include? params['Body'].downcase
-      send_message Grocery.all.active.pluck(:name).join("\n")
+    @user = User.find_by phone: params['From'].to_i.to_s
+    if @user.blank?
+      send_message 'Noodles doesn’t recognize your number. Add it: https://getnoodl.es/groceries'
+    elsif ['groceries', 'grocery list', 'list'].include? params['Body'].downcase
+      send_message @user.groceries.active.pluck(:name).join("\n") + 'https://getnoodl.es/groceries'
     else
-      Grocery.create name: params['Body'], user: User.first
+      Grocery.create name: params['Body'].to_s.squish, user: @user
       send_message 'Saved! (Reply List for all)'
     end
     head :no_content
