ee284ca3a19f6468bc889d88a82dc77cecce100b
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-11-26 21:38:51 -0500

    Hide Allrecipes placeholder images

--- app/helpers/explore/allrecipes.rb
+++ app/helpers/explore/allrecipes.rb
@@ -1,12 +1,17 @@
 class AllrecipesSearchScraper
+  PLACEHOLDERS = [
+    'http://images.media-allrecipes.com/images/44555.png',
+    'http://images.media-allrecipes.com/userphotos/250x250/0.jpg'
+  ]
+  SELECTOR = 'section.grid article.grid-col--fixed-tiles:not(#dfp_container):not(.video-card):not(.hub-card)'
+
   def scrape(q)
     scraper = Mechanize.new
     scraper.history_added = proc { sleep 0.25 }
 
     results = []
     url = 'http://allrecipes.com/search/results/?sort=re&wt=' + q
-    selector = 'section.grid article.grid-col--fixed-tiles:not(#dfp_container):not(.video-card):not(.hub-card)'
-    raw_results = scraper.get(url).search(selector)
+    raw_results = scraper.get(url).search(SELECTOR)
     raw_results.each do |item|
       next if item.at_css('div').attr('class').match('article-card').present?
       result = {}
@@ -23,8 +28,7 @@ class AllrecipesSearchScraper
     results.delete_if { |item| !item['url'] || !item['title'] }.compact!
     results.each do |item|
       item['description'] = item['description'].to_s.truncate(164)
-      placeholder = 'http://images.media-allrecipes.com/userphotos/250x250/0.jpg'
-      item['image'] = '' if item['image'].to_s.match?(placeholder)
+      item['image'] = '' if PLACEHOLDERS.include? item['image'].to_s
     end
     results
   end
