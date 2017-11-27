fde44e9f15da718ab6a79eb43762c2b5915a0a55
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-11-27 09:49:10 -0500

    Add form to groceries blankslate

--- app/javascript/components/GroceriesBlankSlate.js
+++ app/javascript/components/GroceriesBlankSlate.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import BlankSlate from './BlankSlate'
+import GroceriesForm from './GroceriesForm'
 
 const GroceriesBlankSlate = () => (
-  <BlankSlate margin="mvn">
+  <BlankSlate margin="mtm mbn">
     <h3 className="mvn">No groceries yet.</h3>
     <p className="mts mbn">
       Keep a list of all the groceries you need to buy here.
     </p>
+    <GroceriesForm makeSuggestions />
   </BlankSlate>
 )
 
