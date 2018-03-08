18eabe971b780fe87c773fec5726d671b564b5c6
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-03-08 14:17:16 -0500

    Update meta scripts

--- meta/css-stats.js
+++ meta/css-stats.js
@@ -1,12 +1,11 @@
-
 const getCss = require('get-css')
 const CleanCSS = require('clean-css')
 const Parker = require('parker/lib/Parker')
 const ParkerMetrics = require('parker/metrics/All')
 
-getCss('http://localhost:3000/explore', { timeout: 2000 }).then((response) => {
-  const css = response.links[0].css
-  const minified = new CleanCSS().minify(response.links[0].css).styles
+getCss('http://localhost:5100/explore', { timeout: 2000 }).then(res => {
+  const css = res.links[0].css
+  const minified = new CleanCSS().minify(res.links[0].css).styles
   const parker = new Parker(ParkerMetrics)
   console.log(parker.run(minified.toString()))
 })
--- meta/embed.js
+++ meta/embed.js
@@ -1,8 +1,7 @@
-
 const fs = require('fs')
 const Pageres = require('pageres')
 
 new Pageres({ delay: 2 })
-  .src('http://localhost:3000/embed', [ '350x225' ], { filename: 'Embed' })
+  .src('http://localhost:5100/embed', ['350x225'], { filename: 'Embed' })
   .dest('./meta/results')
   .run()
