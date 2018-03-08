3d7692b19c7559c98a06e1f330b1338762e4acfd
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-03-08 13:58:06 -0500

    Test Explore results

--- test/helpers/explore_helper_test.rb
+++ test/helpers/explore_helper_test.rb
@@ -4,19 +4,19 @@ class ExploreHelperTest < ActionView::TestCase
   include ExploreHelper
 
   test 'nyt explore results' do
-    assert_not_nil find_explore_results('nyt', 'pancakes')
+    assert_not_empty find_explore_results('nyt', 'pancakes')
   end
 
   test 'epicurious explore results' do
-    assert_not_nil find_explore_results('epicurious', 'pancakes')
+    assert_not_empty find_explore_results('epicurious', 'pancakes')
   end
 
   test 'allrecipes explore results' do
-    assert_not_nil find_explore_results('allrecipes', 'pancakes')
+    assert_not_empty find_explore_results('allrecipes', 'pancakes')
   end
 
   test 'explore preview' do
-    data = { 'title': 'Pancakes', 'description': '', 'ingredients': ['2 cups flour', '2 eggs'], 'instructions': '' }
+    data = { title: 'Pancakes', description: '', ingredients: ['2 cups flour', '2 eggs'], instructions: '' }
     recipe = prepare_explore_preview(data)
     assert_not_nil recipe
     assert_equal recipe[:title], 'Pancakes'
