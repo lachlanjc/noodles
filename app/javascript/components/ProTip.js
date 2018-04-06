e28c897967791354b71bd4f87538c30ca561f5f4
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-04-05 23:18:33 -0400

    Update tips

--- app/javascript/components/ProTip.js
+++ app/javascript/components/ProTip.js
@@ -6,13 +6,15 @@ const tips = [
   'Tap the lightbulb icon next to search for a random recipe.',
   'Use Cook Mode to check off ingredients as you cook.',
   'Tap the heart icon to mark recipes as Favorites and find them faster.',
-  'Save recipes from the web with the Clip icon in the right corner.',
+  'Save recipes from the web with the Import button.',
   'Share a recipe with anyone from the menu on the recipe page.',
   'Download a PDF of a recipe from the menu on the recipe page.',
   'Get quick access to recipes you’ve cooked recently in the sidebar.',
+  'Get quick access to your Grocery List in the sidebar.',
   'Use the font size controls to enlarge text in Cook Mode.',
+  'Hover an ingredient and click the button to add it to your Grocery List.',
   'Try adding ingredients to your Grocery List over SMS.',
-  'Don’t like something? Tap the Feedback tab and let me know.'
+  'What could be better about Noodles? Tap the Feedback tab and let me know.'
 ]
 const getTip = () => tips[_.random(0, tips.length - 1)]
 
