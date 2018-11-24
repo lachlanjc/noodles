9b44751fd3fb67d06a13804defedea0f4e3e5d8b
Lachlan Campbell <lachlan@hackclub.com>
2018-11-24 16:36:48 -0500

    Update ProTips

--- app/javascript/components/ProTip.js
+++ app/javascript/components/ProTip.js
@@ -3,18 +3,18 @@ import Icon from './Icon'
 
 const tips = [
   'Create a new recipe super quickly by searching with its title.',
-  'Tap the lightbulb icon next to search for a random recipe.',
+  'Open a random recipe with the ✨ icon next to Search.',
   'Use Cook Mode to check off ingredients as you cook.',
-  'Tap the heart icon to mark recipes as Favorites and find them faster.',
+  'Tap the heart icon to mark recipes as Favorites & find them faster.',
   'Save recipes from the web with the Import button.',
   'Share a recipe with anyone from the menu on the recipe page.',
   'Download a PDF of a recipe from the menu on the recipe page.',
   'Get quick access to recipes you’ve cooked recently in the sidebar.',
   'Get quick access to your Grocery List in the sidebar.',
   'Use the font size controls to enlarge text in Cook Mode.',
-  'Hover an ingredient and click the button to add it to your Grocery List.',
+  'Hover an ingredient & click the button to add it to your Grocery List.',
   'Try adding ingredients to your Grocery List over SMS.',
-  'What could be better about Noodles? Tap the Feedback tab and let me know.'
+  'Wish something was different in Noodles? Tap the Feedback tab & let me know.'
 ]
 
 const ProTip = () => (
@@ -28,7 +28,9 @@ const ProTip = () => (
     }}
   >
     <Icon glyph="emoji" size={28} className="dib grey-3 mrs relative" />
-    <strong style={{ marginRight: 6 }}>ProTip!</strong>
+    <strong className="sans" style={{ marginRight: 4 }}>
+      ProTip!
+    </strong>
     <span children={tips[_.random(0, tips.length - 1)]} />
   </footer>
 )
