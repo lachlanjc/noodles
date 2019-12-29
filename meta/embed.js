cb559fb3f085746bbe091257a8150947a3b1b1ca
Lachlan Campbell <lachlan@hackclub.com>
2019-12-28 19:44:46 -0500

    Remove meta scripts

--- meta/embed.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const Pageres = require('pageres')
-
-new Pageres({ delay: 2 })
-  .src('http://localhost:5100/embed', ['350x225'], { filename: 'Embed' })
-  .dest('./meta/results')
-  .run()
