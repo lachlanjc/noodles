e267627647bcfcaf3f5d5a90617ac24b870c52c5
Lachlan Campbell <lachlanjc@hey.com>
2020-12-17 15:19:31 -0500

    Design tweaks

--- app/javascript/components/CollectionBlankSlate.js
+++ app/javascript/components/CollectionBlankSlate.js
@@ -9,24 +9,24 @@ const heading = <h3 className="mvn">No recipes in this collection yet.</h3>
 
 const CollectionBlankSlate = ({ id, pub = true }) =>
   pub ? (
-    <BlankSlate className="bg-white" margin="man" children={heading} />
+    <BlankSlate className="bg-white" margin="mtn" children={heading} />
   ) : (
-    <BlankSlate className="bg-white" margin="man">
-      {heading}
-      <p className="mvm">
-        Create a new recipe, or add an existing one from its page.
+      <BlankSlate className="bg-white" margin="mtn">
+        {heading}
+        <p className="mvm">
+          Create a new recipe, or add an existing one from its page.
       </p>
-      <footer className="flex fjc">
-        <Button
-          color="blue"
-          href={`/recipes/new?collection=${id}`}
-          children="New recipe"
-        />
-        <Spacer x={16} />
-        <Button href="/recipes" color="grey-3" children="Find a recipe" />
-      </footer>
-    </BlankSlate>
-  )
+        <footer className="flex fjc">
+          <Button
+            color="blue"
+            href={`/recipes/new?collection=${id}`}
+            children="New recipe"
+          />
+          <Spacer x={16} />
+          <Button href="/recipes" color="grey-3" children="Find a recipe" />
+        </footer>
+      </BlankSlate>
+    )
 
 CollectionBlankSlate.propTypes = {
   id: PropTypes.number.isRequired,
