5e4be3669a7cde1a0e06f15ee1e295a5297ce934
Lachlan Campbell <lachlanjc@hey.com>
2021-03-12 03:58:30 -0500

    Disable allrecipes explore testing

--- test/controllers/explore_controller_test.rb
+++ test/controllers/explore_controller_test.rb
@@ -21,13 +21,12 @@ class ExploreControllerTest < ActionController::TestCase
     assert_not_empty assigns(:results)
   end
 
-  test 'should get allrecipes results' do
-    get :results, params: { src: 'allrecipes', q: 'pancakes' }
-    assert_response :success
-    assert_not_empty assigns(:results)
-  end
+  # test 'should get allrecipes results' do
+  #   get :results, params: { src: 'allrecipes', q: 'pancakes' }
+  #   assert_response :success
+  #   assert_not_empty assigns(:results)
+  # end
 
-=begin
   test 'should get nyt preview' do
     get :preview, params: { url: 'http://cooking.nytimes.com/recipes/1012732-bacon-lettuce-and-plum-sandwiches' }
     assert_response :success
@@ -48,14 +47,13 @@ class ExploreControllerTest < ActionController::TestCase
     assert_equal '6 servings', r.serves
   end
 
-  test 'should get allrecipes preview' do
-    get :preview, params: { url: 'http://allrecipes.com/recipe/18433/irresistible-pecan-pie' }
-    assert_response :success
-    r = assigns(:recipe)
-    assert_not_nil r
-    assert_match 'white sugar', r.ingredients
-    assert_match 'Preheat', r.instructions
-    assert_equal '12 servings', r.serves
-  end
-=end
+  # test 'should get allrecipes preview' do
+  #   get :preview, params: { url: 'http://allrecipes.com/recipe/18433/irresistible-pecan-pie' }
+  #   assert_response :success
+  #   r = assigns(:recipe)
+  #   assert_not_nil r
+  #   assert_match 'white sugar', r.ingredients
+  #   assert_match 'Preheat', r.instructions
+  #   assert_equal '12 servings', r.serves
+  # end
 end
