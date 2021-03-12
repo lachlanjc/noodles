d8f05d1020e89367fdf7f38c6702db60a7b6883a
Lachlan Campbell <lachlanjc@hey.com>
2021-03-12 03:46:57 -0500

    Hack together new recipe scraper

--- app/helpers/scraping_helper.rb
+++ app/helpers/scraping_helper.rb
@@ -1,42 +1,33 @@
+require 'json'
+
 # Provide all services related to scraping the web for a recipe.
 module ScrapingHelper
   include RecipesHelper
 
-  def master_scrape(link)
-    host = find_domain(link)
-    path = find_path(link)
+  def master_scrape(url)
+    host = find_domain(url)
+    path = find_path(url)
 
-    url = link
-    url.gsub!('http://', 'https://')
-    url.gsub!(/\/print/, '') if host.match?(/allrecipes\.com/)
-
-    page = safely { Mechanize.new.get(url).content }
+  begin
+    # safely silence: Mechanize::ResponseCodeError do
+      content = Mechanize.new.get("https://util.getnoodl.es/api/scrape?url=#{url}").content
+      recipe = JSON.parse content
+    # end
+  rescue
+    page = Mechanize.new.get(url).content
     data = Hangry.parse page
+    recipe = process_recipe_page(url, page, data)
+  end
 
-    return unless data.name.present? || host =~ /allrecipes|marthastewart|epicurious|bonappetit|foodandwine|food52/
-    create_recipe_item process_recipe_page(url, page, data)
+    return unless recipe&.dig('name')
+    create_recipe_item recipe
   end
 
   # Process recipe pages
   def process_recipe_page(url, page, data)
     host = find_domain(url).to_s
     doc = Nokogiri::HTML::DocumentFragment.parse(page)
-    if host.match? 'nytimes.com'
-      data = process_nyt_page!(data, doc)
-    elsif host.match? 'epicurious.com'
-      data = process_epicurious_page!(data, doc)
-    elsif host.match? 'allrecipes.com'
-      data = process_allrecipes_page!(data, doc)
-    elsif host.match? 'foodandwine.com'
-      data = process_fw_page!(data, doc)
-    elsif host.match? 'bonappetit.com'
-      data = process_ba_page!(data, doc)
-    elsif host.match? 'marthastewart.com'
-      data.instructions = doc.css('.directions-list .directions-item').text
-    elsif host.match? 'driscolls.com'
-      data.instructions = doc.css('#recipe-content #instructions').text
-    end
-    if data.to_h.values.reject(&:blank?).empty?
+    if host.match? 'nytimes.com' || data.to_h.values.reject(&:blank?).empty?
       data = process_schema_for data, doc
     elsif data.instructions.to_s.match(/\n/).blank?
       data = process_blog_page! data, doc
@@ -50,80 +41,17 @@ module ScrapingHelper
       data.name = values['name']
       data.description = values['description']&.squish
       data.ingredients = values['recipeIngredient']
-      data.instructions = values['recipeInstructions']
+      data.instructions = values['recipeInstructions']&.map { |o| o['text'] }
       data.yield = values['recipeYield'].to_s.remove('Makes ')
       data.author = values['author']['name']
     end
     data
   end
 
-  # Fixes for NYT Cooking
-  def process_nyt_page!(data, doc)
-    s = [
-      '[itemprop=description] p:first-of-type',
-      '.recipe-ingredients li',
-      '.recipe-steps li',
-      '.recipe-subhead span[itemprop=author]',
-      'ul.recipe-notes li'
-    ]
-    data.description = doc.css(s[0])&.text
-    data.ingredients = doc.css(s[1])&.to_a&.map(&:text)
-    data.instructions = doc.css(s[2])&.to_a&.map(&:text)
-    data.author = doc.search(s[3])&.text
-    data.notes = doc.css(s[4])&.to_a&.map(&:text).join("\n")
-    data
-  end
-
-  # Fixes for Epicurious
-  def process_epicurious_page!(data, doc)
-    data.name = doc.css('h1[itemprop=name]').text
-    data.description = doc.css('.dek[itemprop=description]').text
-    data.ingredients = []
-    data.instructions = []
-    doc.css('.ingredients-info [itemprop=ingredients]').each do |n|
-      data.ingredients.push n.text
-    end
-    n = ':not(#chefNotes)'
-    i = "#{inst} li#{n}, #{inst} > p#{n}"
-    data.instructions = doc.css(i).text.split(/\n/).map(&:strip).flatten.uniq.join("\n")
-    data.yield = doc.css('[itemprop=recipeYield]').text
-    data
-  end
-
-  # Fixes for Allrecipes
-  def process_allrecipes_page!(data, doc)
-    data.name = doc.css('.recipe-summary h1[itemprop=name]')&.text
-    data.ingredients = doc.css('li span[itemprop="recipeIngredient"]')&.map(&:text)
-    data.yield = doc.css('meta[itemprop="recipeYield"]')&.attr('content')&.text
-    data.yield += ' servings' if data.yield.present?
-    data
-  end
-
-  # Fixes for Food & Wine
-  def process_fw_page!(data, doc)
-    data.name = doc.css('h1')[0].text
-    data.description = doc.css('.recipe-summary')&.text
-    data.ingredients = doc.css('.ingredients li').to_a.map(&:text)
-    data.instructions = doc.css("#{inst} p:last-child").to_a.map(&:text)
-    data.yield = doc.css('.recipe-meta-item-body:last-of-type')&.text
-    data
-  end
-
-  # Fixes for Bon Appetit
-  def process_ba_page!(data, doc)
-    data.name = doc.css('h1[itemprop=name]').text
-    data.description = doc.css('.dek--basically').text
-    data.ingredients = doc.css('.ingredients li').to_a.map(&:text)
-    data.instructions = doc.css('.steps .step').to_a.map(&:text)
-    data.author = doc.css('.contributor-recipe-author .contributor-name')&.text
-    data.yield = doc.css('.recipe__header__servings')&.text
-    data
-  end
-
-  # Support some sites (mainly blogs)
-  def process_blog_page!(data, doc)
+   # Support some sites (mainly blogs)
+   def process_blog_page!(data, doc)
     data.instructions = doc.css(inst).to_a.map(&:text)
-    if data&.ingredients.empty?
+    if !data&.ingredients
       s = '.wprm-recipe-ingredient-group li[itemprop=recipeIngredient]'
       data.ingredients = doc.css(s).to_a.map(&:text)
     end
@@ -133,11 +61,11 @@ module ScrapingHelper
   # Basic scaffolding for recipe item
   def create_recipe_item(data)
     {
-      title: data.name.to_s.squish.truncate(255),
-      description: data.description.to_s.squish,
-      ingredients: data.ingredients,
-      instructions: data.instructions,
-      serves: data.yield.to_s.capitalize.remove('Servings:',).strip
+      title: data['name'].to_s.squish.truncate(255),
+      description: data['description'].to_s.squish,
+      ingredients: data['ingredients'],
+      instructions: data['instructions'],
+      serves: data['yield'].to_s.capitalize.remove('Servings:',).strip
     }
   end
 
@@ -169,11 +97,10 @@ module ScrapingHelper
     ingredients = ingredients.split(/\s\s+/) if ingredients.is_a? String
     ingredients.delete_if { |item| item.to_s.squish!.gsub(/\s\s+/, ' ').length < 3 }
     # Each of the steps is now in an array.
-    ingredients.each do |item|
+    ingredients.map do |item|
       # Remove custom lists or line breaks
-      item.gsub!(/^\-|\*\s?/, '').to_s.gsub!(/\s\s+/, ' ').to_s.squish!.capitalize
+      item.gsub(/^\-|\*\s?/, '').to_s.gsub(/\s\s+/, ' ').to_s.squish.capitalize
     end
-    ingredients
   end
 
   # Write a list of ingredients
@@ -199,11 +126,10 @@ module ScrapingHelper
     steps.delete_if do |step|
       step.to_s.squish.length < 3 || step.to_s.match('Preparation')
     end
-    steps.each do |step|
+    steps.map do |step|
       # Remove custom numbering or line breaks
-      step.gsub!(/^\w?\d\.?/, '').to_s.squish!
+      step.gsub(/^\w?\d\.?/, '').to_s.squish
     end
-    steps
   end
 
   # Generate Markdown based on instructions
@@ -220,15 +146,15 @@ module ScrapingHelper
   def inst
     '[itemprop=recipeInstructions]'
   end
-
+  
   def find_domain(url)
     URI(url).host.to_s.match(/[^\.]+\.\w+$/).to_s
   end
-
+  
   def find_domain_name(url)
     find_domain(url).match(/(.+)\.\w+/)[1].to_s
   end
-
+  
   def find_path(url)
     URI(url).path.to_s
   end
--- test/helpers/scraping_helper_test.rb
+++ test/helpers/scraping_helper_test.rb
@@ -11,9 +11,9 @@ class ScrapingHelperTest < ActionView::TestCase
     assert_not_nil @recipe[:instructions]
   end
 
-  # test 'nyt cooking import' do
-  #   check_recipe('Bacon, Lettuce and Plum Sandwiches', 'http://cooking.nytimes.com/recipes/1012732-bacon-lettuce-and-plum-sandwiches')
-  # end
+  test 'nyt cooking import' do
+    check_recipe('Bacon, Lettuce and Plum Sandwiches', 'http://cooking.nytimes.com/recipes/1012732-bacon-lettuce-and-plum-sandwiches')
+  end
 
   test 'epicurious import' do
     check_recipe('Sheet-Pan Grilled Cheese', 'http://www.epicurious.com/recipes/food/views/sheet-pan-grilled-cheese-56390006')
@@ -23,15 +23,15 @@ class ScrapingHelperTest < ActionView::TestCase
     check_recipe('Chocolate Chip Cookie Ice Cream Bars', 'https://www.foodandwine.com/recipes/chocolate-chip-cookie-ice-cream-bars')
   end
 
-  # test 'allrecipes import' do
-  #   check_recipe('Peppered Shrimp Alfredo', 'http://allrecipes.com/recipe/133128/peppered-shrimp-alfredo/')
-  # end
-
-  test 'food52 import' do
-    check_recipe('Carrot-Pineapple Cake with Cream Cheese Frosting', 'https://food52.com/recipes/38008-carrot-pineapple-cake-with-cream-cheese-frosting')
+  test 'allrecipes import' do
+    check_recipe('Peppered Shrimp Alfredo', 'http://allrecipes.com/recipe/133128/peppered-shrimp-alfredo/')
   end
 
-  # test 'bon appetit import' do
-  #   check_recipe('Chocolate Chunk–Pumpkin Seed Cookies', 'https://www.bonappetit.com/recipe/chocolate-chunk-pumpkin-seed-cookies')
+  # test 'food52 import' do
+  #   check_recipe('Carrot-Pineapple Cake with Cream Cheese Frosting', 'https://food52.com/recipes/38008-carrot-pineapple-cake-with-cream-cheese-frosting')
   # end
+
+  test 'bon appetit import' do
+    check_recipe('Chocolate Chunk–Pumpkin Seed Cookies', 'https://www.bonappetit.com/recipe/chocolate-chunk-pumpkin-seed-cookies')
+  end
 end
